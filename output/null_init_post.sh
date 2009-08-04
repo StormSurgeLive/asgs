37b76e2b1c1f6ed6aaa777bd2f82deba89f7cc82
Jason Fleming <jason.fleming@seahorsecoastal.com>
2009-08-04 14:25:48 +0000

    added two postprocessing scripts that intentionally don't do anything except copy results to /dev/null, to make it so that more tests can be run without interrupting downstream results that have already been created

---
+++ output/null_init_post.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# null_init_post.sh 
+# 
+# Does nothing.
+# 
+# Copyright(C) 2008, 2009 Jason Fleming
+#
+# This file is part of the ADCIRC Surge Guidance System (ASGS).
+#
+# The ASGS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ASGS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the ASGS.  If not, see <http://www.gnu.org/licenses/>.
+#
+ASGSADVISORYDIR=$1
+STORM=$2
+YEAR=$3
+ADVISORY=$4
+HOSTNAME=$5
+#
+#mkdir -p $POSTDIR/${STORM}${YEAR}/${ADVISORY}
---
+++ output/null_post.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# null_post.sh 
+# 
+# Copies output from the ASGS to /dev/null.
+# 
+# Copyright(C) 2008, 2009 Jason Fleming
+#
+# This file is part of the ADCIRC Surge Guidance System (ASGS).
+#
+# The ASGS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ASGS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the ASGS.  If not, see <http://www.gnu.org/licenses/>.
+#
+ASGSADVISORYDIR=$1
+STORM=$2
+YEAR=$3
+ADVISORY=$4
+HOSTNAME=$5
+ENSTORM=$6
+#
+POSTDIR=/dev/null
+#
+#POSTADVISORYDIR=$POSTDIR/${STORM}${YEAR}/${ADVISORY}
+cp $ASGSADVISORYDIR/al${STORM}${YEAR}.fst $POSTDIR
+cp $ASGSADVISORYDIR/bal${STORM}${YEAR}.dat $POSTDIR
+metalink=`ls ${YEAR}${STORM}${ADVISORY}?_w???o???v????r???`
+cp $metalink $POSTDIR
+cp $ASGSADVISORYDIR/$ENSTORM/fort.22 $POSTDIR
+cp $ASGSADVISORYDIR/$ENSTORM/fort.22.meta $POSTDIR
+cp $ASGSADVISORYDIR/$ENSTORM/fort.61 $POSTDIR
+cp $ASGSADVISORYDIR/$ENSTORM/maxele.63 $POSTDIR
+cp $ASGSADVISORYDIR/$ENSTORM/maxwvel.63 $POSTDIR
