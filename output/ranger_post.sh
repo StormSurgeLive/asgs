30d48038a4349351eda4e9e8bfa3e17ef3c1ab3f
Robert Weaver <rjweaver@fit.edu>
2009-10-16 20:01:54 +0000

    commit new changes to ranger specific postprocessing scripts

--- output/ranger_post.sh
+++ output/ranger_post.sh
@@ -24,51 +24,88 @@
    ADVISORY=$5
    HOSTNAME=$6
    ENSTORM=$7
-   COLDSTARTDATE=$8
+   CSDATE=$8
    HSTIME=$9
-   GRIDFILE=$10
-   OUTPUTDIR=$11
+   GRIDFILE=${10}
+   OUTPUTDIR=${11}
+   SYSLOG=${12}
    #
-   . ${CONFIG} # 
+   . ${CONFIG} # grab all static config info
    #
-   # 
-   mkdir ${ADVISDIR}/${ENSTORM}/plots
+   # grab storm class and name from file
+   STORMNAME=`cat nhcClassName` 
+   STORMNAME=${STORMNAME}" "${YEAR}
    # transpose elevation output file so that we can graph it with gnuplot
-   perl ${OUTPUTDIR}/station_transpose.pl --filetotranspose elevation --controlfile ${ADVISDIR}/${ENSTORM}/fort.15 --stationfile ${ADVISDIR}/${ENSTORM}/fort.61 --format space --coldstartdate ${COLDSTARTDATE} --gmtoffset -5 --timezone CDT --units english
+   perl ${OUTPUTDIR}/station_transpose.pl --filetotranspose elevation --controlfile ${ADVISDIR}/${ENSTORM}/fort.15 --stationfile ${ADVISDIR}/${ENSTORM}/fort.61 --format space --coldstartdate $CSDATE --gmtoffset -5 --timezone CDT --units english
    # transpose wind velocity output file so that we can graph it with gnuplot
-   perl ${OUTPUTDIR}/station_transpose.pl --filetotranspose windvelocity --controlfile ${ADVISDIR}/${ENSTORM}/fort.15 --stationfile ${ADVISDIR}/${ENSTORM}/fort.72 --format space --vectorOutput magnitude --coldstartdate ${COLDSTARTDATE} --gmtoffset -5 --timezone CDT --units english
-   # plot elevation data with gnuplot
-   perl ${OUTPUTDIR}/autoplot.pl --filetoplot fort.61_transpose.txt --plotType elevation --plotdir ${ADVISDIR}/${ENSTORM}/plots --outputdir ${OUTPUTDIR} --timezone CDT --units english --stormname ${YEAR}${STORM} --enstorm $ENSTORM --advisory $ADVISORY --datum NAVD88
-   # plot wind speed data with gnuplot 
-   perl ${OUTPUTDIR}/autoplot.pl --filetoplot fort.72_transpose.txt --plotType windvelocity --plotdir ${ADVISDIR}/${ENSTORM}/plots --outputdir ${OUTPUTDIR} --timezone CDT --units english --stormname ${YEAR}${STORM} --enstorm $ENSTORM --advisory $ADVISORY --datum NAVD88
+   perl ${OUTPUTDIR}/station_transpose.pl --filetotranspose windvelocity --controlfile ${ADVISDIR}/${ENSTORM}/fort.15 --stationfile ${ADVISDIR}/${ENSTORM}/fort.72 --format space --vectorOutput magnitude --coldstartdate $CSDATE --gmtoffset -5 --timezone CDT --units english
+   # now create csv files that can easily be imported into excel
+   perl ${OUTPUTDIR}/station_transpose.pl --filetotranspose elevation --controlfile ${ADVISDIR}/${ENSTORM}/fort.15 --stationfile ${ADVISDIR}/${ENSTORM}/fort.61 --format comma --coldstartdate $CSDATE --gmtoffset -5 --timezone CDT --units english
+   perl ${OUTPUTDIR}/station_transpose.pl --filetotranspose windvelocity --controlfile ${ADVISDIR}/${ENSTORM}/fort.15 --stationfile ${ADVISDIR}/${ENSTORM}/fort.72 --format comma --vectorOutput magnitude --coldstartdate $CSDATE --gmtoffset -5 --timezone CDT --units english
+   # 
+   # switch to plots directory
    initialDirectory=`pwd`;
+   mkdir ${ADVISDIR}/${ENSTORM}/plots
+   mv *.txt *.csv ${ADVISDIR}/$ENSTORM/plots
    cd ${ADVISDIR}/$ENSTORM/plots
+   # plot elevation data with gnuplot
+   perl ${OUTPUTDIR}/autoplot.pl --filetoplot fort.61_transpose.txt --plotType elevation --plotdir ${ADVISDIR}/${ENSTORM}/plots --outputdir ${OUTPUTDIR} --timezone CDT --units english --stormname "$STORMNAME" --enstorm $ENSTORM --advisory $ADVISORY --datum NAVD88
+   # plot wind speed data with gnuplot 
+   perl ${OUTPUTDIR}/autoplot.pl --filetoplot fort.72_transpose.txt --plotType windvelocity --plotdir ${ADVISDIR}/${ENSTORM}/plots --outputdir ${OUTPUTDIR} --timezone CDT --units english --stormname "$STORMNAME" --enstorm $ENSTORM --advisory $ADVISORY --datum NAVD88
    for plotfile in `ls *.gp`; do
       gnuplot $plotfile
    done
    for plotfile in `ls *.ps`; do
       pngname=${plotfile%.ps}.png
-      convert $plotfile $pngname
+      ${IMAGEMAGICKPATH}/convert -rotate 90 $plotfile $pngname
    done
-   # now create csv files that can easily be imported into excel
-   perl ${OUTPUTDIR}/station_transpose.pl --filetotranspose elevation --controlfile ${ADVISDIR}/${ENSTORM}/fort.15 --stationfile ${ADVISDIR}/${ENSTORM}/fort.61 --format comma --coldstartdate ${COLDSTARTDATE} --gmtoffset -5 --timezone CDT --units english
-   perl ${OUTPUTDIR}/station_transpose.pl --filetotranspose windvelocity --controlfile ${ADVISDIR}/${ENSTORM}/fort.15 --stationfile ${ADVISDIR}/${ENSTORM}/fort.72 --format comma --vectorOutput magnitude --coldstartdate ${COLDSTARTDATE} --gmtoffset -5 --timezone CDT --units english
-   # 
-   tar cvzf ${ADVISDIR}/${ENSTORM}/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}-plots.tar.gz *.png *.csv
+   tar cvzf ${ADVISDIR}/${ENSTORM}/${YEAR}${STORM}.${ADVISORY}.plots.tar.gz *.png *.csv
    cd $initialDirectory
 #
-# ---------------------------------------------------------
-#  now run POST_SCRIPT for KMZ GIS and JPGs
-# --------------------------------------------------------
-      ${OUTPUTDIR}/POSTPROC_KMZGIS/POST_SCRIPT.sh  $ADVISDIR $OUTPUTDIR $STORM $YEAR $ADVISORY $HOSTNAME $ENSTORM $GRIDFILE 
+# FigGen32.exe (called below) calls the 'convert' program, and the path is 
+# not configurable there, so let's see if we can get the program to work
+# by adding the imagemagick path to the path before calling that program
+export PATH=$PATH:$IMAGEMAGICKPATH 
 #
-# now copy the files to the appropriate locations
+#  now create the Google Earth, jpg, and GIS output files
+   ${OUTPUTDIR}/POSTPROC_KMZGIS/POST_SCRIPT.sh $ADVISDIR $OUTPUTDIR $STORM $YEAR $ADVISORY $HOSTNAME $ENSTORM $GRIDFILE 
+
+# -----------------------------------
+# RANGER SPECIFIC
+# ----------------------------------
+
+#  now copy the files to the appropriate locations
       OUTPUTPREFIX1=${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}-KMZ_GIS
       OUTPUTPREFIX2=${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}-plots
-#
+    mv  ${ADVISDIR}/${ENSTORM}/${YEAR}${STORM}.${ADVISORY}.plots.tar.gz ${ADVISDIR}/${ENSTORM}/$OUTPUTPREFIX2.tar.gz
+
+      if  [! -d  $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}]
+       then
+      mkdir  $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}
+        chmod 766 $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}
+      fi
+      cp $OUTPUTPREFIX1.tar.gz $OUTPUTPREFIX2.tar.gz fort.22 fort.22.meta $ADVISDIR/al${STORM}${YEAR}.fst $ADVISDIR/bal${STORM}${YEAR}.dat fort.61 maxele.63 maxwvel.63 $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}/
+
+ #    tar -xzf $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}/$OUTPUTPREFIX1.tar.gz -C $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}/
+ #    mv $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}/${OUTPUTPREFIX1}_files/* $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}/
+ #    rmdir $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}/${OUTPUTPREFIX1}_files
+
+       chmod 644  $DATAPOSTDIR/${STORM}_${YEAR}_${ENSTORM}_${ADVISORY}/*
+
+ #
     ${OUTPUTDIR}/scp_files_ranger.exp ${OUTPUTDIR} $ADVISDIR $RESULTSHOST $RESULTSPATH $RESULTSPROMPT $RESULTSUSER $RESULTSPASSWORD $HOSTNAME $ENSTORM $OUTPUTPREFIX1 $OUTPUTPREFIX2 > scp.log &
 
     sleep 60
 
+# now create a timeseries animation KMZ
+#  cd $POSTPROC_DIR/TimeseriesKMZ/
+#
+#    ln -fs $INPUTFILE ./
+#    ln -fs $ADVISDIR/$ENSTORM/fort.14 ./
+#    ln -fs $ADVISDIR/$ENSTORM/fort.63 ./
+#    ln -fs $ADVISDIR/$ENSTORM/fort.74 ./
+#
+#  perl make_JPG.pl --outputdir $POSTPROC_DIR --gmthome $GMTHOME --storm ${STORM} --year ${YEAR} --adv $ADVISORY --n $NORTH --s $SOUTH --e $EAST --w $WEST --outputprefix $OUTPUTPREFIX
+#
+#     ${OUTPUTDIR}/POSTPROC_KMZGIS/TimeseriesKMZ/
 
-    
