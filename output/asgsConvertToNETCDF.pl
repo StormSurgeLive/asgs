a131324f87e79777f121f891292ff8ce1ae10753
Jason Fleming <jason.fleming@seahorsecoastal.com>
2012-05-10 14:36:52 +0000

    Changes to implement new file types in run.properties.

--- output/asgsConvertToNETCDF.pl
+++ output/asgsConvertToNETCDF.pl
@@ -2,7 +2,7 @@
 # #!/home/thorpe/local/bin/perl -W
 #
 # wrapper for pqinsert an adcirc solution file into local LDM product queue
-# calls 
+# calls
 # pqinsert_adcirc_for_scoop_sub.pl fort.61
 # pqinsert_adcirc_for_scoop_sub.pl fort.63
 # pqinsert_adcirc_for_scoop_sub.pl fort.64
@@ -38,21 +38,21 @@ sub stderrMessage($$);
 # set the OpenDAP prefix:  Change this if/when we change where opendap.renci.org points.
 my $openDAPPrefix;
 
-# the new name of the run.properties file as stored in OpenDap. Set below and 
+# the new name of the run.properties file as stored in OpenDap. Set below and
 # used in the call to sendEmailMessage
 my $runPropertiesFileName;
 
 # SET the email list!
 my $toList="howard\@renci.org, jason.fleming\@seahorsecoastal.com, nc.cera.renci\@gmail.com, xwang25\@uncc.edu, lyu8\@uncc.edu, aidong.lu\@uncc.edu";
 
-my %types=("fort.15"         => "fort15" , 
-           "fort.61"         => "statelev", 
-           "fort.63"         => "elev", 
-           "fort.64"         => "dvel", 
-           "fort.71"         => "statatmpress", 
-           "fort.72"         => "statwvel", 
-           "fort.73"         => "atmpress", 
-           "fort.74"         => "wvel", 
+my %types=("fort.15"         => "fort15" ,
+           "fort.61"         => "statelev",
+           "fort.63"         => "elev",
+           "fort.64"         => "dvel",
+           "fort.71"         => "statatmpress",
+           "fort.72"         => "statwvel",
+           "fort.73"         => "atmpress",
+           "fort.74"         => "wvel",
            "fort.67"         => "hots67",
            "fort.68"         => "hots",
            "maxele.63"       => "maxelev",
@@ -72,6 +72,30 @@ my %types=("fort.15"         => "fort15" ,
            "swan_HS_max.63"  => "maxhsign",
            "run.properties"  => "run.properties",
            "stationOut.tar"  => "stations");
+
+my %ids_descs;
+$ids_descs{"fort.61"} = "Water Surface Elevation Stations";
+$ids_descs{"fort.62"} = "Water Current Velocity Stations";
+$ids_descs{"fort.63"} = "Water Surface Elevation";
+$ids_descs{"fort.64"} = "Water Current Velocity";
+$ids_descs{"fort.71"} = "Barometric Pressure Stations";
+$ids_descs{"fort.72"} = "Wind Velocity Stations";
+$ids_descs{"fort.73"} = "Barometric Pressure";
+$ids_descs{"fort.74"} = "Wind Velocity";
+$ids_descs{"maxele.63"} = "Maximum Water Surface Elevation";
+$ids_descs{"maxvel.63"} = "Maximum Current Speed";
+$ids_descs{"maxwvel.63"} = "Maximum Wind Speed";
+$ids_descs{"minpr.63"} = "Minimum Barometric Pressure";
+$ids_descs{"maxrs.63"} = "Maximum Wave Radiation Stress";
+$ids_descs{"swan_DIR.63"} = "Mean Wave Direction";
+$ids_descs{"swan_DIR_max.63"} = "Maximum Mean Wave Direction";
+$ids_descs{"swan_HS.63"} = "Significant Wave Height";
+$ids_descs{"swan_HS_max.63"} = "Maximum Significant Wave Height";
+$ids_descs{"swan_TMM10.63"} = "Mean Wave Period";
+$ids_descs{"swan_TMM10_max.63"} = "Maximum Mean Wave Period";
+$ids_descs{"swan_TPS.63"} = "Peak Wave Period";
+$ids_descs{"swan_TPS_max.63"} = "Maximum Peak Wave Period";
+
 #
 my @files = qw( fort.61 fort.63 fort.64 fort.71 fort.72 fort.73 fort.74 maxele.63 maxrs.63 maxwvel.63 minpr.63 swan_HS.63 swan_HS_max.63 swan_TMM10.63 swan_TMM10_max.63 swan_TPS.63 swan_TPS_max.63 swan_DIR.63 swan_DIR_max.63 timeofmaxele.63 );
 my @compressionOnlyFiles = qw( fort.15 stationOut.tar );
@@ -81,18 +105,18 @@ GetOptions(
     "ppdir=s" => \$cmdLinePPDIR,
     "griddir=s" => \$GRIDDIR,
     "opendapbasedir=s" => \$OPENDAPBASEDIR,
-    "insertfile" => \$InsertFile, 
+    "insertfile" => \$InsertFile,
     "deletefiles" => \$DeleteFiles,
     "sendnotification" => \$SendNotification,
     "inundationmask=s" => \$inundationmask,
-    "copyfile" => \$CopyFile   
+    "copyfile" => \$CopyFile
           );
 #
 if (defined $cmdLinePPDIR) {
    $PPDIR = $cmdLinePPDIR;
 } elsif (defined $envPPDIR) {
    $PPDIR = $envPPDIR;
-} 
+}
 #stderrMessage("DEBUG","Path to executables (PPDIR) is set to ".$PPDIR.".");
 #stderrMessage("DEBUG","Path to mesh file (GRIDDIR) is set to '$GRIDDIR'.");
 #stderrMessage("DEBUG","Path to OPenDAP data (OPENDAPBASEDIR) is set to '$OPENDAPBASEDIR'.");
@@ -142,9 +166,9 @@ $openDAPPrefix="http://opendap.renci.org:1935/thredds/catalog/$model/$ADCIRCgrid
 #stderrMessage("DEBUG","openDAPPrefix: $openDAPPrefix");
 
 unless (-e $openDAPDirectory) {
-   system("mkdir -p  $openDAPDirectory"); 
+   system("mkdir -p  $openDAPDirectory");
 }
-# write the opendap directory that the files were copied to so that we 
+# write the opendap directory that the files were copied to so that we
 # know where they went
 unless ( open(OPENDAPPATH,">opendappath.log") ) {
    stderrMessage("ERROR","Could not open the file opendappath.log for writing: $!.");
@@ -155,15 +179,15 @@ close(OPENDAPPATH);
 
 if (!defined $prodID) {
    stderrMessage("ERROR","prodID not found in run.properties file.");
-   die; 
+   die;
 }
 if (!defined $ADCIRCgrid) {
-   stderrMessage("ERROR","ADCIRCgrid not found in run.properties file."); 
+   stderrMessage("ERROR","ADCIRCgrid not found in run.properties file.");
    die;
 }
 my @filesProcessed;
 #
-# if this run corresponds to a particular tropical cyclone, we will want to 
+# if this run corresponds to a particular tropical cyclone, we will want to
 # publish the ATCF formatted hindcast and forecast data
 if ( $stormnumber ne "00" ) {
    my $forecast_atcf = "../al".$stormnumber.$year.".fst";
@@ -200,31 +224,31 @@ foreach my $file (@files) {
    stderrMessage("INFO","Processing $file.");
    if (!-e $file){
       stderrMessage("INFO","$file was not found. It will not be processed.");
-      next;  
+      next;
    }
    if (-e "$file.nc.gz") {
       stderrMessage("INFO","Deleting $file.nc.gz ...");
-      system("rm -rf $file.nc.gz"); 
+      system("rm -rf $file.nc.gz");
    }
    #stderrMessage("DEBUG","Converting $file to $file.nc (netCDF).");
-   $status=`$PPDIR/convert_adc_native_2_netCDF -y $rsy -m $rsm -d $rsd -h $rsh $file`;   
+   $status=`$PPDIR/convert_adc_native_2_netCDF -y $rsy -m $rsm -d $rsd -h $rsh $file`;
    # The conversion program writes a lot of info to stdout ... can't use this
    # as a status indicator
    #if ( $status == 1 ) {
-   #   stderrMessage("ERROR","Conversion program '$PPDIR/convert_adc_native_2_netCDF' returned '$status'. The file $file will not be processed."); 
+   #   stderrMessage("ERROR","Conversion program '$PPDIR/convert_adc_native_2_netCDF' returned '$status'. The file $file will not be processed.");
    #   next;
-   #}                           
-   # if this is a fort.63 || fort.64 file, 
+   #}
+   # if this is a fort.63 || fort.64 file,
    # append netcdf version of grid to .nc file
    #   print  "Appending netcdf grid file to $fnc ...\n";
    #   if ( $f eq "fort.63" || $f eq "fort.64" || $f eq "maxele.63" || $f eq "timeofmaxele.63" ||
-   #       $f eq "swan_HS.63" || $f eq "swan_HS_max.63" || 
-   #       $f eq "swan_TM01.63" || $f eq "swan_TM01_max.63" || 
-   #       $f eq "swan_TPS.63" || $f eq "swan_TPS_max.63" || 
+   #       $f eq "swan_HS.63" || $f eq "swan_HS_max.63" ||
+   #       $f eq "swan_TM01.63" || $f eq "swan_TM01_max.63" ||
+   #       $f eq "swan_TPS.63" || $f eq "swan_TPS_max.63" ||
    #       $f eq "swan_DIR.63" || $f eq "swan_DIR_max.63"){
    #
    #      my $temp=$ADCIRCNCGDLOC;
-   #      $temp=~s/<GRIDNAME>/$ADCIRCgrid/;  
+   #      $temp=~s/<GRIDNAME>/$ADCIRCgrid/;
    #      die "$temp not found." if (!-e $temp);
    #      $com="$NCODIR/ncks --quiet --append $temp $fnc >& /dev/null";
    #      print "$com\n";
@@ -239,22 +263,30 @@ foreach my $file (@files) {
       if ( -e $file ) {
          # append depth data and inundation masks
          $status = `ncks --quiet --append $PPDIR/$inundationmask $file.nc`;
-         # calculate inundation level     
-         $status = `ncap2 --overwrite -S $PPDIR/produceInundationData.scr $file.nc $file.nc`;    
+         # calculate inundation level
+         $status = `ncap2 --overwrite -S $PPDIR/produceInundationData.scr $file.nc $file.nc`;
       }
    }
    #
    # compress netcdf files
    #stderrMessage("DEBUG","Gzipping $file.nc ... ");
-   $status=`gzip --force $file.nc`; 
+   $status=`gzip --force $file.nc`;
    if ( $status ne "" ) {
       stderrMessage("ERROR","Compression of $file.nc failed: $status. The file $file will not be processed.");
-      next; 
+      next;
    }
    $myProductID = $prodID;
    $myProductType = $types{$file};
    $myProductID=~s/<field>/$myProductType/;
    push(@filesProcessed,$file.".nc.gz",$myProductID);
+   #
+   # update the hash that represents the run.properties file to
+   # show that the ascii file has been converted to renci-netcdf, and
+   # the name of the new file
+   my $filenamekey = $ids_descs{$file} . " File Name";
+   $RP{$filenamekey} = $myProductID;
+   my $fileformatkey = $ids_descs{$file} . " Format";
+   $RP{$fileformatkey} = "renci-netcdf";
    #
    my $shpPrefix = $myProductID;
    # Some special processing for the maxele files. We'd like to
@@ -269,20 +301,20 @@ foreach my $file (@files) {
    #             $result = `tar cfz $shpTarFile $shpPrefix.{shp,shx,prj,dbf} `;
    #            my $shpZipFile = "$shpPrefix.zip";
    #            $result = `zip -j $shpZipFile $shpPrefix.{shp,shx,prj,dbf} `;
-   
+
    #            if ($InsertFile) {
    #               print "Inserting $shpZipFile as $shpZipFile ... \n";
    #               &pqi("$shpZipFile",$shpZipFile);
    #            }
-   
+
    #            if ($DeleteFiles) {
-   #               system("rm -rf $shpPrefix.{shp,shx,prj,dbf}"); 
+   #               system("rm -rf $shpPrefix.{shp,shx,prj,dbf}");
    #            }
    #         }
-   # We may want to delete the files once they are processed. 
+   # We may want to delete the files once they are processed.
    if ($DeleteFiles) {
-       #system("rm -rf $file"); 
-      system("rm -rf $file.nc.gz"); 
+       #system("rm -rf $file");
+      system("rm -rf $file.nc.gz");
    }
 }
 #
@@ -292,7 +324,7 @@ foreach my $file (@compressionOnlyFiles) {
    stderrMessage("INFO","Processing $file.");
    if (!-e $file){
       stderrMessage("INFO","$file was not found. It will not be processed.");
-      next;  
+      next;
    }
    $myProductID = $prodID;
    $myProductType = $types{$file};
@@ -318,6 +350,10 @@ if (-e "run.properties") {
    $runPropertiesFileName = $myProductID;
 }
 #
+# write out the hash that represents the run.properties file
+# with the updated names and formats
+&writeHashToFile("run.properties",":",%RP);
+#
 # now copy and/or insert processed files as requested
 my $num_files = @filesProcessed;
 if ($CopyFile) {
@@ -341,7 +377,7 @@ for (my $i=0; $i<($num_files/2); $i++ ) {
 if ($SendNotification) {
    my $subject = "ADCIRC NCFS POSTED for $fullDate";
    if ( $windtag =~ /vortex/ ) {
-      $subject .= " (TROPICAL CYCLONE)"; 
+      $subject .= " (TROPICAL CYCLONE)";
    }
    my $httpPathName=$openDAPPrefix;
    $httpPathName=~s/catalog/fileServer/;
@@ -349,7 +385,7 @@ if ($SendNotification) {
    if ( defined $windtag ) {
       $path_suffix =  "$windtag/$year/$mon/$mday/$cycle/";
    } else {
-      $path_suffix =  "$year/$mon/$mday/$cycle/";    
+      $path_suffix =  "$year/$mon/$mday/$cycle/";
    }
    my $message = <<END;
 
@@ -379,7 +415,7 @@ print 0;
 ##########
 
 #
-#  Prints a message to stderr, annotated with the date, the specified 
+#  Prints a message to stderr, annotated with the date, the specified
 #  severity level, and the name of this script.
 sub stderrMessage($$) {
    my $level = shift;
@@ -393,7 +429,7 @@ sub stderrMessage($$) {
 }
 #
 # ReadFileAsHash reads the contents of a file into
-# a perl hash.  It assumes that there are 2 fields per 
+# a perl hash.  It assumes that there are 2 fields per
 # line in the file, separated by a character (like :).
 # The line is split on this char, and the left and right
 # fields are put into the key and value for the hash entry.
@@ -405,7 +441,10 @@ sub ReadFileAsHash
    # read file as a hash, split on $_[1];
    my $fname = $_[0];
 #   print "reading hash file $fname\n";
-   open(FIL,"<$fname");
+   unless ( open(FIL,"<$fname") ) {
+      stderrMessage("ERROR","Could not open the file $fname for reading: $!.");
+      die;
+   }
    while (<FIL>){
       ($k,$v)=split /$_[1]/;
       chomp($k);chomp($v);
@@ -416,6 +455,21 @@ sub ReadFileAsHash
    }
    close(FIL);
    return %H;
+}
+
+sub writeHashToFile
+{
+   my $fname = shift;
+   my $separator = shift;
+   my %hashToWrite = shift;
+   unless ( open(FIL,">$fname") ) {
+      stderrMessage("ERROR","Could not open the file $fname for writing: $!.");
+      die;
+   }
+   foreach my $key (keys(%hashToWrite)) {
+      printf FIL "$key $separator $hashToWrite{$key}\n";
+   }
+   close(FIL)
 }
 
 sub PrintHash
