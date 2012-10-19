408affb9d1d09bbffe36cda8ff093add583b87a9
Jason Fleming <jason.fleming@seahorsecoastal.com>
2012-10-19 20:12:00 +0000

    Corrected an issue in opening the swan screen file; added code to calculate the DD:HH:MM:SS of wall clock time if the starting and ending date/times are provided on the command line.

--- output/swan_convergence_analysis.pl
+++ output/swan_convergence_analysis.pl
@@ -47,14 +47,43 @@ my @num_iterations;  # how many iterations there were for each swan time step
 my $iteration = 0;
 my $total_iterations = 0;
 my $total_steps = 0;
+my $startwc = "null";  # starting wall clock time e.g. 2012-10-08 12:23:21
+my $finishwc = "null"; # finishing wall clock time e.g. 2012-10-08 14:59:56
 #
 GetOptions(
            "swanscreen=s" => \$swanscreen,
            "swanprint=s" => \$swanprint,
            "gmtoffset=s" => \$gmtoffset,
            "timezone=s" => \$timezone,
+           "startwc=s" => \$startwc,
+           "finishwc=s" => \$finishwc
            );
-
+#
+# compute the wall clock time required by the run if start and finish 
+# times were provided on the command line
+if ( $startwc ne "null" && $finishwc ne "null" ) {
+   $startwc =~ /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/;
+   my $s_year = $1;
+   my $s_mon = $2;
+   my $s_day = $3;
+   my $s_hour = $4;
+   my $s_min = $5;
+   my $s_sec = $6;
+   $finishwc =~ /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/;
+   my $f_year = $1;
+   my $f_mon = $2;
+   my $f_day = $3;
+   my $f_hour = $4;
+   my $f_min = $5;
+   my $f_sec = $6;
+   # get difference
+   (my $ddays, my $dhrs, my $dmin, my $dsec)
+           = Date::Pcalc::Delta_DHMS(
+                $s_year,$s_mon,$s_day,$s_hour,$s_min,$s_sec,
+                $f_year,$f_mon,$f_day,$f_hour,$f_min,$f_sec);
+   my $wall_clock_time = sprintf("%02d:%02d:%02d:%02d",$ddays,$dhrs,$dmin,$dsec);
+   print "The elapsed wall clock time was $wall_clock_time.\n";
+}
 unless(open(SWANSCREEN,"<$swanscreen")) {
    stderrMessage("ERROR","Could not open $swanscreen for reading.");
    exit 1;
