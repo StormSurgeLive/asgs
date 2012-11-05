ed1682424da1721c40747d1ee71f0b020c1f13fd
Jason Fleming <jason.fleming@seahorsecoastal.com>
2012-11-05 17:22:10 +0000

    Removed extraneous code from adcircMaxColumnar.f90.

--- output/adcircMaxColumnar.f90
+++ output/adcircMaxColumnar.f90
@@ -29,6 +29,9 @@
 !
 ! Example of compiling adcircMaxColumnar.f90 with gfortran:
 ! gfortran -o adcircMaxColumnar.x -cpp -ffree-form -ffree-line-length-none -I. adcircMaxColumnar.f90 
+! 
+! Example of compiling adcircMaxColumnar.f90 with ifort on blueridge:
+! ifort -o adcircMaxColumnar.x -DHAVE_NETCDF4 -DNETCDF_CAN_DEFLATE -i-dynamic -I/shared/apps/RHEL-5/x86_64/NetCDF/netcdf-4.1.2-gcc4.1-ifort/include -L/shared/apps/RHEL-5/x86_64/NetCDF/netcdf-4.1.2-gcc4.1-ifort/lib adcircMaxColumnar.f90 -lnetcdf -lnetcdff -lz
 
 include 'adcmesh.f90'
 
@@ -37,74 +40,28 @@ include 'adcmesh.f90'
       USE adcmesh
       USE netcdf
       IMPLICIT NONE
-      CHARACTER(120)                :: InputFile, OutputFile, AttFile
-      character(120)                :: datenum
-      character(120),   allocatable :: att(:,:)
+      CHARACTER(120)                :: InputFile
       character(1000)               :: Line
-      character(1)                  :: JunkC, Tadj
       DOUBLE PRECISION              :: DefaultValue, FillValue=-99999.0d0
-      double precision              :: temp1, temp2, SnapR, Interval, time(1)
       DOUBLE PRECISION, ALLOCATABLE :: Global1(:), Global2(:), Global3(:)
-      integer                       :: yy, mo, dd, hh, mi
       integer                       :: natt, i, j, k, N, SS, NumNodes, NumSnaps
-      integer                       :: NumNodesNonDefault, SnapI
-      integer                       :: unitnumber, nCol
-      integer, allocatable          :: iopt(:)         ! files to convert
       integer                       :: nopt         ! actual number of files to convert
       integer                       :: menuOpt   ! user's selection
       integer                       :: argcount  ! number of command line arguments
       integer                       :: iargc     ! function to return command line arguments
       character(2048)               :: cmdlineopt ! command line option
       character(2048)               :: cmdlinearg ! content of command line argument
-      logical                       :: useNetCDF4 ! .true. if user wants netcdf classic model
                                                   ! files formatted in hdf5 format
-      logical                       :: meshonly   ! .true. if user just wants to conver the mesh
-      integer                       :: ncFileType
-      integer                       :: NC_ID
-      INTEGER                       :: NC_DimID(2)
-      INTEGER                       :: NC_Count(2)
-      INTEGER                       :: NC_Start(2)
-
-      integer                       :: NC_DimID_time
-      integer                       :: NC_DimID_single
-
-      integer                       :: NC_VarID_time
-      integer                       :: NC_VarID_zeta
-      integer                       :: NC_VarID_u_vel
-      integer                       :: NC_VarID_v_vel
-      integer                       :: NC_VarID_maxele
-      integer                       :: NC_VarID_maxwvel
-      integer                       :: NC_VarID_p
-      integer                       :: NC_VarID_windx
-      integer                       :: NC_VarID_windy
-      integer                       :: NC_VarID_dir
-      integer                       :: NC_VarID_hs
-      integer                       :: NC_VarID_tmm10
-      integer                       :: NC_VarID_tps
-      integer, parameter            :: version = 4
-      integer                       :: num_components ! variable components for netcdf4 compression
-      integer                       :: varid(3) ! varids for netcdf4 compression
       character(80) :: dummy
       real(8) :: dataval
       integer :: nodenum
-
-
-      ! initializations
-      deg2rad = 2*pi/360.0
-      meshFileName = "null"
-      attFile = "null"
-      menuOpt = 0
-      useNetCDF4 = .false.
-      meshonly = .false.
       !
-      !write(6,*) "INFO: adcirc2netcdf version ",version,"."
+      ! initializations   
+      meshFileName = "null"
+      !
       ! Report netcdf version
       write(6,*) "INFO: adcirc2netcdf was compiled with the following netcdf library: ",trim(nf90_inq_libvers())
 
-      ! jgf: Process command line options; can be used along with menu choices;
-      ! if command line options provide all needed input, menu will not
-      ! be presented to user; programs with command line options are
-      ! slightly easier to automate than menu-based programs
       argcount = iargc() ! count up command line options
       if (argcount.gt.0) then
          i=0
@@ -112,59 +69,15 @@ include 'adcmesh.f90'
             i = i + 1
             call getarg(i, cmdlineopt)
             select case(trim(cmdlineopt))
-               case("--netcdf4")
-                  useNetCDF4 = .true.
-                  write(6,*) "INFO: Processing ",trim(cmdlineopt),"."
-               case("--meshonly")
-                  meshonly = .true.
-                  menuOpt = 14
-                  write(6,*) "INFO: Processing ",trim(cmdlineopt),"."
                case("--meshfile")
                   i = i + 1
                   call getarg(i, cmdlinearg)
                   write(6,*) "INFO: Processing ",trim(cmdlineopt)," ",trim(cmdlinearg),"."
                   meshFileName = trim(cmdlinearg)
-               case("--attfile")
-                  i = i + 1
-                  call getarg(i, cmdlinearg)
-                  write(6,*) "INFO: Processing ",trim(cmdlineopt)," ",trim(cmdlinearg),"."
-                  attFile = trim(cmdlinearg)
                case("--datafile")
                   i = i + 1
                   call getarg(i, cmdlinearg)
                   write(6,*) "INFO: Processing ",trim(cmdlineopt)," ",trim(cmdlinearg),"."
-                  select case(trim(cmdlinearg))
-                     case("fort.63")
-                        menuOpt = 1
-                     case("fort.64")
-                        menuOpt = 2
-                     case("fort.73")
-                        menuOpt = 3
-                     case("fort.74")
-                        menuOpt = 4
-                     case("maxele.63")
-                        menuOpt = 5
-                     case("swan_DIR.63")
-                        menuOpt = 6
-                     case("swan_HS.63")
-                        menuOpt = 7
-                     case("swan_TMM10.63")
-                        menuOpt = 8
-                     case("swan_TPS.63")
-                        menuOpt = 9
-                     case("adcirc")
-                        menuOpt = 10
-                     case("swan")
-                        menuOpt = 11
-                     case("adcirc_swan")
-                        menuOpt = 12
-                     case("fort.14")
-                        menuOpt = 14
-                     case("maxwvel.63")
-                        menuOpt = 15
-                     case default
-                        write(6,*) "WARNING: Command line argument '",TRIM(cmdlinearg),"' was not recognized."
-                  end select
                   InputFile = trim(cmdlinearg)
                case default
                   write(6,*) "WARNING: Command line option '",TRIM(cmdlineopt),"' was not recognized."
@@ -175,7 +88,7 @@ include 'adcmesh.f90'
       ! Load fort.14
       call read14()
 
-      ! load maxele.63
+      ! load ascii maxele.63
       call openFileForRead(63, trim(InputFile))
       read(63,*) dummy  ! header 1
       read(63,*) dummy  ! header 2
@@ -186,9 +99,17 @@ include 'adcmesh.f90'
          global1(nodeNum) = dataval
       end do
       open(11,file='maxele.txt',status='replace',action='write')
+      write(11,*) '-----------------------------------------------------'
+      write(11,*) 'Longitude    Latitude    Max Water     Total Water'
+      write(11,*) ' (decimal     (decimal    Elevation     Depth'
+      write(11,*) '  degrees)    degrees)    (ft NAVD88)   (ft NAVD88)'
+      write(11,*) '-----------------------------------------------------'
       do i=1,np
-         write(11,*) xyd(1,i),xyd(2,i),global1(i),global1(i)+xyd(3,i)
+         if (global1(i).gt.-9999.d0) then
+            write(11,11) xyd(1,i),xyd(2,i),global1(i)*3.2808399,(global1(i)+xyd(3,i))*3.2808399
+         endif
       end do
+11    format(2(F10.6,'  '),2(E10.5,'  '))
       close(11)
       stop
       end program adcircMaxColumnar
