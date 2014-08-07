02d28effe3dae2e879d9c7d6ebd73ac99e70ab0b
Jason Fleming <jason.fleming@seahorsecoastal.com>
2014-08-07 14:30:22 +0000

    Updated post processing to record sea_surface_height_above_geoid to run.properties file.
    Former-commit-id: 0c6424dd2b0e9d5e4e88ff12fe7918b5fe26e096

--- output/corps_post.sh
+++ output/corps_post.sh
@@ -72,6 +72,23 @@ echo "intendedAudience : $INTENDEDAUDIENCE" >> run.properties
 echo "asgs : ng" >> run.properties 2>> $SYSLOG
 echo "enstorm : $ENSTORM" >> run.properties 2>> $SYSLOG
 #
+# record the sea_surface_height_above_geoid nodal attribute to the
+# run.properties file
+isUsed=`grep -c sea_surface_height_above_geoid fort.15`
+if [[ $isUsed = 0 ]]; then
+   # this nodal attribute is not being used; report this to run.properties file
+   echo "sea_surface_height_above_geoid : null" >> run.properties
+else
+   # get the line number where the start of this nodal attribute is specified
+   # in the header of the fort.13 (nodal attributes) file
+   linenum=`grep --line-number --max-count 1 sea_surface_height_above_geoid fort.13 | awk 'BEGIN { FS=":" } { print $1 }'`
+   # get the actual default value, which is specified three lines after the
+   # the name of the nodal attribute in the fort.13 header
+   datumOffsetDefaultValueLine=`expr $linenum + 3`
+   datumOffsetDefaultValue=`awk -v linenum=$datumOffsetDefaultValueLine 'NR==linenum { print $0 }' fort.13`
+   echo "sea_surface_height_above_geoid : $datumOffsetDefaultValue" >> run.properties
+fi
+#
 #  R E F O R M A T T I N G
 #
 #
