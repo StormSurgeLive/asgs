79ecaca69403192f895bba6b4612b2dfd8d51282
jasonfleming <jason.fleming@seahorsecoastal.com>
2017-06-20 16:38:53 -0400

    Added utility scripts for generating offset surfaces.

---
+++ input/data_assimilation/stationProcessor.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+te=2592000; ts=2502588; adv=init
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2613600; ts=2524188; adv=01
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2635200; ts=2545788; adv=02
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2656800; ts=2567388; adv=03
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2678400; ts=2588988; adv=04
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2700000; ts=2610588; adv=05
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2710800; ts=2621388; adv=06
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2721600; ts=2632188; adv=07
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2743200; ts=2653788; adv=08
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2743200; ts=2653788; adv=09
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2764800; ts=2675388; adv=10
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2786400; ts=2696988; adv=11
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2808000; ts=2718588; adv=12
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2829600; ts=2740188; adv=13
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2851200; ts=2761788; adv=14
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2872800; ts=2783388; adv=15
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2894400; ts=2804988; adv=16
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2916000; ts=2826588; adv=17
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2937600; ts=2848188; adv=18
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2959200; ts=2869788; adv=19
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=2980800; ts=2891388; adv=20
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3002400; ts=2912988; adv=21
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3024000; ts=2934588; adv=22
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3045600; ts=2956188; adv=23
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3067200; ts=2977788; adv=24
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3088800; ts=2999388; adv=25
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3110400; ts=3020988; adv=26
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3132000; ts=3042588; adv=27
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3153600; ts=3064188; adv=28
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3175200; ts=3085788; adv=29
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3196800; ts=3107388; adv=30
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3218400; ts=3128988; adv=31
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3240000; ts=3150588; adv=32
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3261600; ts=3172188; adv=33
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3283200; ts=3193788; adv=34
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3304800; ts=3215388; adv=35
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3326400; ts=3236988; adv=36
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3348000; ts=3258588; adv=37
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3369600; ts=3280188; adv=38
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3391200; ts=3301788; adv=39
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3412800; ts=3323388; adv=40
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3434400; ts=3344988; adv=41
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3456000; ts=3366588; adv=42
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3477600; ts=3388188; adv=43
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3499200; ts=3409788; adv=44
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3520800; ts=3431388; adv=45
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3542400; ts=3452988; adv=46
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
+te=3564000; ts=3474588; adv=47
+~/asgs/2014stable/output/stationProcessor.x --datafile init.msl.fort.61.nc --datafile 16.msl.fort.61.nc --datafile 47.nowcast.msl.fort.61.nc --timesec-end $te --timesec-start $ts --stationfile ~/offset_source_stations.txt --strict-time-range --outputfile $adv.end.processedStations.dat
