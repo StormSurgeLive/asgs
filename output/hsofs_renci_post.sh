7584ded276bfc5f4632fc1aac40f987114ce4fce
jasonfleming <jason.fleming@seahorsecoastal.com>
2017-08-03 11:26:50 -0400

    Fixed issue where the non-completion of the 10m wind contours would prevent the raw 10m wind data from getting posted to opendap.

--- output/hsofs_renci_post.sh
+++ output/hsofs_renci_post.sh
@@ -258,6 +258,7 @@ FILES=(`ls *.nc al${STORM}${YEAR}.fst bal${STORM}${YEAR}.dat fort.15 fort.22 CER
 #-------------------------------------------------------------------
 # @jasonfleming: Hack in a notification email once the bare minimum files
 # needed by CERA have been posted. 
+ceraNonPriorityFiles=( `ls endrisinginun.63.nc everdried.63.nc fort.64.nc fort.68.nc fort.71.nc fort.72.nc fort.73.nc initiallydry.63.nc inundationtime.63.nc maxinundepth.63.nc maxrs.63.nc maxvel.63.nc minpr.63.nc rads.64.nc swan_DIR.63.nc swan_DIR_max.63.nc swan_TMM10.63.nc swan_TMM10_max.63.nc` )
 ceraPriorityFiles=(`ls run.properties maxele.63.nc fort.63.nc fort.61.nc fort.15 fort.22`)
 if [[ $ceraContoursAvailable = yes ]]; then
    ceraPriorityFiles=( ${ceraPriorityFiles[*]} "CERA.tar" )
@@ -269,14 +270,13 @@ if [[ $WAVES = on ]]; then
    ceraPriorityFiles=( ${ceraPriorityFiles[*]} `ls swan_HS_max.63.nc swan_TPS_max.63.nc swan_HS.63.nc swan_TPS.63.nc` )
 fi
 dirWind10m=$ADVISDIR/${ENSTORM}Wind10m
-if [[ -d $dirWind10m && $wind10mContoursFinished = yes ]]; then
+if [[ -d $dirWind10m ]]; then
    ceraPriorityFiles=( ${ceraPriorityFiles[*]} `ls wind10m.maxwvel.63.nc wind10m.fort.74.nc` )
+   ceraNonPriorityFiles=( ${ceraNonPriorityFiles[*]} `ls maxwvel.63.nc fort.74.nc` )
 else
    ceraPriorityFiles=( ${ceraPriorityFiles[*]} `ls maxwvel.63.nc fort.74.nc` )
 fi
-ceraPriorityFiles=( ${ceraPriorityFiles[*]} "sendNotification" )
-ceraPriorityFiles=( ${ceraPriorityFiles[*]} `ls endrisinginun.63.nc everdried.63.nc fort.64.nc fort.68.nc fort.71.nc fort.72.nc fort.73.nc initiallydry.63.nc inundationtime.63.nc maxinundepth.63.nc maxrs.63.nc maxvel.63.nc minpr.63.nc rads.64.nc swan_DIR.63.nc swan_DIR_max.63.nc swan_TMM10.63.nc swan_TMM10_max.63.nc` )
-FILES=( ${ceraPriorityFiles[*]} )
+FILES=( ${ceraPriorityFiles[*]} "sendNotification" ${ceraNonPriorityFiles[*]} )
 #
 # For each opendap server in the list in ASGS config file.
 primaryCount=0
