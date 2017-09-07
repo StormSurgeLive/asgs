638f654182512543a17c3f898d3a8109063884a4
Jason Fleming <jason.fleming@seahorsecoastal.com>
2017-09-07 09:21:10 -0500

    Updated for Irma on queenbee.

--- config/2017/asgs_config_nam_queenbee_LA_v12h-WithUpperAtch_setFlux_noWaves.sh
+++ config/2017/asgs_config_nam_queenbee_LA_v12h-WithUpperAtch_setFlux_noWaves.sh
@@ -28,7 +28,7 @@
 # Fundamental
 
 INSTANCENAME=v12hSetFluxNoWaves  # "name" of this ASGS process
-COLDSTARTDATE=2017072200   # calendar year month day hour YYYYMMDDHH24
+COLDSTARTDATE=2017073100   # calendar year month day hour YYYYMMDDHH24
 HOTORCOLD=coldstart      # "hotstart" or "coldstart"
 LASTSUBDIR=null          # path to previous execution (if HOTORCOLD=hotstart)
 HINDCASTLENGTH=30.0      # length of initial hindcast, from cold (days)
