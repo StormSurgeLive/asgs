f6cebee9bcdfb829d8112605b0db1691af378e8c
Jason Fleming <jason.fleming@seahorsecoastal.com>
2018-03-14 08:44:21 -0500

    Committing latest config file for LA v17a mesh.

--- config/2018/asgs_config_nam_swan_queenbee_LAv17a.sh
+++ config/2018/asgs_config_nam_swan_queenbee_LAv17a.sh
@@ -28,7 +28,8 @@
 # Fundamental
 
 INSTANCENAME=dailyv17a   # "name" of this ASGS process
-COLDSTARTDATE=2017120100 # calendar year month day hour YYYYMMDDHH24
+#COLDSTARTDATE=2017120100 # calendar year month day hour YYYYMMDDHH24
+COLDSTARTDATE=2018020900 # calendar year month day hour YYYYMMDDHH24
 HOTORCOLD=coldstart      # "hotstart" or "coldstart"
 LASTSUBDIR=null          # path to previous execution (if HOTORCOLD=hotstart)
 HINDCASTLENGTH=30.0      # length of initial hindcast, from cold (days)
