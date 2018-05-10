8843d5f895a0a6ec653333c009e99dcbc52baf28
BrianOBlanton <Brian_Blanton@Renci.Org>
2018-05-10 15:16:21 -0400

    tweaks

--- config/2018/asgs_config_nam_hatteras_ec95d.sh
+++ config/2018/asgs_config_nam_hatteras_ec95d.sh
@@ -8,7 +8,8 @@
 # etc)
 #-------------------------------------------------------------------
 #
-# Copyright(C) 2013 Jason Fleming
+# Copyright(C) 2006--2016 Jason Fleming
+# Copyright(C) 2006, 2007 Brett Estrade 
 #
 # This file is part of the ADCIRC Surge Guidance System (ASGS).
 #
@@ -24,149 +25,157 @@
 # You should have received a copy of the GNU General Public License along with
 # the ASGS.  If not, see <http://www.gnu.org/licenses/>.
 #-------------------------------------------------------------------
-
-# Fundamental
-
-INSTANCENAME=bobtest     # "name" of this ASGS process
-COLDSTARTDATE=2018040100 # calendar year month day hour YYYYMMDDHH24
-HOTORCOLD=coldstart      # "hotstart" or "coldstart"
-LASTSUBDIR=null          # path to previous execution (if HOTORCOLD=hotstart)
-HINDCASTLENGTH=30.0      # length of initial hindcast, from cold (days)
-REINITIALIZESWAN=no      # used to bounce the wave solution
+#
+# Fundamental 
+#
+INSTANCENAME=bobtest      # name of this ASGS process (Change this for every new instance)
+COLDSTARTDATE=2018040100              # (date to start cold start from )
+HOTORCOLD=coldstart        # "hotstart" or "coldstart" 
+LASTSUBDIR=null
+HINDCASTLENGTH=30.0       # length of initial hindcast, from cold (days)  
+REINITIALIZESWAN=no       # used to bounce the wave solution
 
 # Source file paths
-H="/home/bblanton/"
-ADCIRCDIR=${H}/ADCIRC/v52release/work # ADCIRC executables
-SCRIPTDIR=${H}/GitHub/renci-unc/asgs        # ASGS executables
-INPUTDIR=${SCRIPTDIR}/input/meshes/ec95d # grid and other input files
-OUTPUTDIR=${SCRIPTDIR}/output # post processing scripts
-PERL5LIB=${SCRIPTDIR}/PERL    # DateCale.pm perl module
+
+ADCIRCDIR=/home/bblanton/ADCIRC/v52release/work
+SCRIPTDIR=/home/bblanton/GitHub/renci-unc/asgs       # ASGS scripts/executables  
+INPUTDIR=${SCRIPTDIR}/input/meshes/ec95d   # dir containing grid and other input files 
+OUTPUTDIR=${SCRIPTDIR}/output # dir containing post processing scripts
+PERL5LIB=${SCRIPTDIR}/PERL    # dir with DateCale.pm perl module
 
 # Physical forcing
 
-BACKGROUNDMET=on     # NAM download/forcing
-TIDEFAC=on           # tide factor recalc
-TROPICALCYCLONE=off  # tropical cyclone forcing
-WAVES=off             # wave forcing
-VARFLUX=off          # variable river flux forcing
+BACKGROUNDMET=on     # [de]activate NAM download/forcing 
+TIDEFAC=on           # [de]activate tide factor recalc 
+TROPICALCYCLONE=off  # [de]activate tropical cyclone forcing (temp. broken)
+WAVES=off            # [de]activate wave forcing 
+VARFLUX=off           # [de]activate variable river flux forcing
 
 # Computational Resources
 
-TIMESTEPSIZE=30.0           # adcirc time step size (seconds)
-SWANDT=1200                 # swan time step size (seconds)
-HINDCASTWALLTIME="01:00:00" # hindcast wall clock time
-ADCPREPWALLTIME="00:05:00"  # adcprep wall clock time, including partmesh
-NOWCASTWALLTIME="01:00:00"  # longest nowcast wall clock time
-FORECASTWALLTIME="01:00:00" # forecast wall clock time
-NCPU=24                     # number of compute CPUs for all simulations
-NCPUCAPACITY=24
+TIMESTEPSIZE=30
+SWANDT=1200
+HINDCASTWALLTIME="01:00:00"   # river inital and tidal spinup time in machine
+ADCPREPWALLTIME="00:05:00"
+NOWCASTWALLTIME="01:00:00"  # must have leading zero, e.g., 05:00:00
+FORECASTWALLTIME="01:00:00" # must have leading zero, e.g., 05:00:00
+NCPU=32
+NUMWRITERS=0
+NCPUCAPACITY=32
 CYCLETIMELIMIT="05:00:00"
-QUEUENAME=null
-SERQUEUE=null
-SCRATCHDIR=/scratch/bblanton/asgs-scratch
+QUEUENAME=batch
+SERQUEUE=single
+ACCOUNT=loni_cera_2018
+SCRATCHDIR=/scratch/bblanton/asgs/
 
 # External data sources : Tropical cyclones
 
-STORM=02                         # storm number, e.g. 05=ernesto in 2006
-YEAR=2012                        # year of the storm
-TRIGGER=rssembedded              # either "ftp" or "rss"
-RSSSITE=filesystem
-FTPSITE=filesystem
-FDIR=${INPUTDIR}/sample_advisories
-HDIR=${INPUTDIR}/sample_advisories
-#RSSSITE=www.nhc.noaa.gov         # site information for retrieving advisories
-#FTPSITE=ftp.nhc.noaa.gov         # hindcast/nowcast ATCF formatted files
-#FDIR=/atcf/afst                  # forecast dir on nhc ftp site
-#HDIR=/atcf/btk                   # hindcast dir on nhc ftp site
+STORM=12  # storm number, e.g. 05=ernesto in 2006 
+YEAR=2013 # year of the storm (useful for historical storms) 
+TRIGGER=rssembedded    # either "ftp" or "rss"
+RSSSITE=www.nhc.noaa.gov 
+FTPSITE=ftp.nhc.noaa.gov  # real anon ftp site for hindcast/forecast files
+FDIR=/atcf/afst     # forecast dir on nhc ftp site 
+HDIR=/atcf/btk      # hindcast dir on nhc ftp site 
 
 # External data sources : Background Meteorology
 
-FORECASTCYCLE="00,12"
+FORECASTCYCLE="00,06,12,18" # (cycle of forecasts, needed)
 BACKSITE=ftp.ncep.noaa.gov          # NAM forecast data from NCEP
 BACKDIR=/pub/data/nccf/com/nam/prod # contains the nam.yyyymmdd files
 FORECASTLENGTH=84                   # hours of NAM forecast to run (max 84)
 PTFILE=ptFile_oneEighth.txt         # the lat/lons for the OWI background met
-ALTNAMDIR="/projects/ncfs/data/asgs5463","/projects/ncfs/data/asgs14174"
+ALTNAMDIR="/scratch/bblanton"
 
 # External data sources : River Flux
 
-RIVERSITE=ftp.nssl.noaa.gov
-RIVERDIR=/projects/ciflow/adcirc_info
+#RIVERSITE=ftp.nssl.noaa.gov
+#RIVERDIR=/projects/ciflow/adcirc_info
+
+RIVERSITE=data.disaster.renci.org
+RIVERDIR=/opt/ldm/storage/SCOOP/RHLRv9-OKU
+RIVERUSER=ldm
+RIVERDATAPROTOCOL=scp
 
 # Input files and templates
 
-GRIDFILE=ec_95d.grd                     # mesh (fort.14) file
-GRIDNAME=ec95d
-CONTROLTEMPLATE=ec_95_fort.15_template # fort.15 template
-ELEVSTATIONS=cera_stations.txt    # or substitute your own stations file
+GRIDFILE=ec_95d.grd
+GRIDNAME=ec_95d   # @jasonfleming 20170814: should be nc6d
+MESHPROPERTIES=${GRIDFILE}.properties
+CONTROLTEMPLATE=ec_95_fort.15_template
+CONTROLPROPERTIES=ec_95_fort.15.properties
+ELEVSTATIONS=cera_stations.txt # change to station file only and have a variable here
 VELSTATIONS=cera_stations.txt
-METSTATIONS=cera_stations.txt
-NAFILE=                                 # no nodal attributes for ec95d
-SWANTEMPLATE=fort.26.ec95.template      # only used if WAVES=on
-RIVERINIT=null                          # this mesh has no rivers ...
-RIVERFLUX=null
-HINDCASTRIVERFLUX=null
-PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz
+METSTATIONS=cera_stations.txt # change to station file only and have a variable here
+NAFILE=null  #  fort.13
+NAPROPERTIES=${NAFILE}.properties
+SWANTEMPLATE=fort.26.ec95.template   # no swan
+RIVERINIT=null  # v6brivers.88 # no 
+RIVERFLUX=null  # v6brivers_fort.20_default 
+HINDCASTRIVERFLUX=null  # v6brivers_fort.20_hc_default
+PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz   # prep all_
 HINDCASTARCHIVE=prepped_${GRIDNAME}_hc_${INSTANCENAME}_${NCPU}.tar.gz
 
 # Output files
 
 # water surface elevation station output
-FORT61="--fort61freq 900.0 --fort61netcdf" 
+FORT61="--fort61freq 300.0 --fort61netcdf"
 # water current velocity station output
-FORT62="--fort62freq 0"                    
+FORT62="--fort62freq 300.0  --fort62netcdf"
 # full domain water surface elevation output
 FORT63="--fort63freq 3600.0 --fort63netcdf" 
 # full domain water current velocity output
-FORT64="--fort64freq 3600.0 --fort64netcdf" 
+FORT64="--fort64freq 3600.0 --fort64netcdf"
 # met station output
-FORT7172="--fort7172freq 3600.0 --fort7172netcdf"           
+FORT7172="--fort7172freq 300.0 --fort7172netcdf"
 # full domain meteorological output
-FORT7374="--fort7374freq 3600.0 --fort7374netcdf"           
+FORT7374="--fort7374freq 3600.0 --fort7374netcdf"
 #SPARSE="--sparse-output"
 SPARSE=""
 NETCDF4="--netcdf4"
-OUTPUTOPTIONS="${SPARSE} ${NETCDF4} ${FORT61} ${FORT62} ${FORT63} ${FORT64} ${FORT7172} ${FORT7374}"
+OUTPUTOPTIONS="${SPARSE} ${FORT61} ${FORT62} ${FORT63} ${FORT64} ${FORT7172} ${FORT7374} ${NETCDF4}" 
 # fulldomain or subdomain hotstart files
-HOTSTARTCOMP=fulldomain                    
+HOTSTARTCOMP=fulldomain
 # binary or netcdf hotstart files
-HOTSTARTFORMAT=netcdf                      
+HOTSTARTFORMAT=binary
 # "continuous" or "reset" for maxele.63 etc files
-MINMAX=reset                               
+MINMAX=reset
 
 # Notification
 
-EMAILNOTIFY=no         # yes to have host HPC platform email notifications
-NOTIFY_SCRIPT=ncfs_nam_notify.sh
+EMAILNOTIFY=no # set to yes to have host platform email notifications
 ems="bblanton@renci.org"
-ACTIVATE_LIST=$ems
-NEW_ADVISORY_LIST=$ems
-POST_INIT_LIST=$ems
-POST_LIST=$ems
-JOB_FAILED_LIST=$ems
-NOTIFYUSER=$ems
-ASGSADMIN=$ems
+NOTIFY_SCRIPT=ncfs_nam_notify.sh
+ACTIVATE_LIST="$ems"
+NEW_ADVISORY_LIST="$ems"
+POST_INIT_LIST="$ems"
+POST_LIST="$ems"
+JOB_FAILED_LIST="$ems" 
+NOTIFYUSER="$ems"
+ASGSADMIN="$ems"
 
 # Post processing and publication
 
-INTENDEDAUDIENCE=general
+INTENDEDAUDIENCE=general # meta data audience
 INITPOST=null_init_post.sh
-POSTPROCESS=ncfs_post.sh
+POSTPROCESS=null_post.sh
 POSTPROCESS2=null_post.sh
-TARGET=hatteras
-OPENDAPHOST=tds.renci.org
-OPENDAPUSER=ncfs
-OPENDAPBASEDIR=/scratch/bblanton/asgs-data
-NUMCERASERVERS=2
-WEBHOST=webserver.hostingco.com
-WEBUSER=remoteuser
-WEBPATH=/home/remoteuser/public_html/ASGS/outputproducts
+
+TDS=(renci_tds)
+TARGET=hatteras  # used in post processing to pick up HPC platform config
+OPENDAPUSER=bblanton         # default value that works for RENCI opendap 
+if [[ $OPENDAPHOST = "fortytwo.cct.lsu.edu" ]]; then
+   OPENDAPUSER=jgflemin  # change this for other Operator running on queenbee
+fi
+# OPENDAPNOTIFY is used by opendap_post.sh and could be regrouped with the 
+# other notification parameters above. 
+OPENDAPNOTIFY="bblanton@renci.org"
 
 # Archiving
 
-ARCHIVE=null_archive.sh
-ARCHIVEBASE=/scratch/bblanton/asgs-scratch
-ARCHIVEDIR=archive
+ARCHIVE=null_archive.sh # if null no data gets save
+ARCHIVEBASE=/scratch/bblanton
+ARCHIVEDIR=${ARCHIVEBASE}/asgs_archive
 
 # Forecast ensemble members
 
@@ -180,6 +189,36 @@ case $si in
    ;;
 0)
    ENSTORM=namforecast
+   ;;
+1)
+   ENSTORM=namforecastWind10m
+   ADCPREPWALLTIME="00:20:00"  # adcprep wall clock time, including partmesh
+   FORECASTWALLTIME="00:20:00" # forecast wall clock time
+   CONTROLTEMPLATE=nv6brivers_explicit_rlevel51.nowindreduction.fort.15_template
+   CONTROLPROPERTIES=${CONTROLTEMPLATE}.properties
+   TIMESTEPSIZE=300.0    # 5 minute time steps
+   #NCPU=31               # so total cpus match with other ensemble members
+   #NUMWRITERS=1          # multiple writer procs might collide
+   WAVES=off             # deactivate wave forcing 
+   # turn off water surface elevation station output
+   FORT61="--fort61freq 0"
+   # turn off water current velocity station output
+   FORT62="--fort62freq 0"
+   # turn off full domain water surface elevation output
+   FORT63="--fort63freq 0"
+   # turn off full domain water current velocity output
+   FORT64="--fort64freq 0"
+   # met station output
+   FORT7172="--fort7172freq 300.0 --fort7172netcdf"
+   # full domain meteorological output
+   FORT7374="--fort7374freq 3600.0 --fort7374netcdf"
+   #SPARSE="--sparse-output"
+   SPARSE=""
+   NETCDF4="--netcdf4"
+   OUTPUTOPTIONS="${SPARSE} ${NETCDF4} ${FORT61} ${FORT62} ${FORT63} ${FORT64} ${FORT7172} ${FORT7374}"
+   # prevent collisions in prepped archives
+   PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz
+   POSTPROCESS=null_post.sh
    ;;
 *)
    echo "CONFIGRATION ERROR: Unknown ensemble member number: '$si'."
