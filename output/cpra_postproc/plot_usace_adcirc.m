987becf4aec269f0f5df9fde006a869216e166ea
mattbilskie <mbilsk3@lsu.edu>
2018-05-24 18:18:47 -0500

    Modified the cpra hydrograph plotting script. The order of the stations in the fort.15/61 does not matter. Added a check to continue generating a hydrograph image if the rivergages site is down.

--- output/cpra_postproc/plot_usace_adcirc.m
+++ output/cpra_postproc/plot_usace_adcirc.m
@@ -1,47 +1,30 @@
-clear all
-close all
+clear all;
+close all;
+clc;
+
+colors = [0.9290 0.6940 0.1250; 175/255 54/255 60/255];
+
+% -------------------------------------------------------------------------
 
 % All time are in UTC. The USACE values are adjusted from CDT to UTC
 % by adding 5 hours.
 
+% -------------------------------------------------------------------------
+
 % Read cpraHydro.info for specific run-time information
-fileID = fopen('cpraHydro.info','r');
-info = textscan(fileID,'%s\n');
-fclose(fileID);
+fid = fopen('cpraHydro.info','r');
+info = textscan(fid,'%s\n');
+fclose(fid);
 storm = info{1}{1}; adcGrid = info{1}{2}; forecastValid = info{1}{3};
 
-% Need to get this in through some input information...
-%dtAdvisory = datenum(2012,8,27,9,0,0);
-dtAdvisory = datenum(forecastValid,'yyyymmddHHMMSS');
+% -------------------------------------------------------------------------
 
-colors = [0.9290 0.6940 0.1250; 175/255 54/255 60/255];
-
-% Loop through the number of ensemble simulations
-numEns = 2;
-ensFileNames = {'fort.61.imeds','fort.61.veerRight50.imeds'};
-for i = 1:numEns
-    % Read in ADCIRC time-series water levels
-    adcData(i) = readIMEDS(char(ensFileNames(i)));
-end
-
-% Must be in the order for the fort.61 file
-adcNames = {'17th St. Outfall Canal',...
-    'Seabrook Complex (IHNC-01)',...
-    'IHNC  Surge Barrier (IHNC-02)',...
-    'West Closure Complex (WBV-90)',...
-    'Hero Canal Stop-Log Gage (WBV-09b)',...
-    'Oakville Sluic Gage (WBV-09a)',...
-    'Bayou Segnette Closure (WBV-16.2)',...
-    'Caernarvon Canal Sector Gate (LPV-149)',...
-    'Bayou Dupre Sector Gate (LPV-149)',...
-    'Western Tie-In (WBV-72/74)',...
-    'Empire Floodgate (NOV-13)',...
-    'Empire Lock (NOV-14)'};
-
-% USACE stations
+% These lists are used in order to look-up the appropriate name based on
+% the USACE Station ID found when parsing the fort.15/fort.61.
+% USACE Station ID
 stations={'85625','76065','76030','76265','82762','82770','82742',...
     '85760','76010','82715','01440','01440'};
-stationsName={'West End',...
+usaceStationsName={'West End',...
     'Seabrook CC - Flood Side',...
     'IHNC Surge Barrier East - Flood Side',...
     'GIWW at West Closure Complex - Flood Side',...
@@ -53,6 +36,43 @@ stationsName={'West End',...
     'Bayou Verret / Western Tie-In Sector Gate - Flood Side',...
     'Mississippi River at Empire',...
     'Mississippi River at Empire'};
+% The stationID in parenthesis must match what is in the fort.51
+cpraStationNames = {'17th St. Outfall Canal (17StCanal)',...
+    'Seabrook Complex (IHNC01)',...
+    'IHNC  Surge Barrier (IHNC02)',...
+    'West Closure Complex (WBV90)',...
+    'Hero Canal Stop-Log Gage (WBV09b)',...
+    'Oakville Sluice Gage (WBV09a)',...
+    'Bayou Segnette Closure (WBV162)',...
+    'Caernarvon Canal Sector Gate (LPV149)',...
+    'Bayou Dupre Sector Gate (LPV144)',...
+    'Western Tie-In (WBV7274)',...
+    'Empire Floodgate (NOV13)',...
+    'Empire Lock (NOV14)'};
+
+% Read in table containg the Station ID and index of the cpra stations as
+% contained in the fort.15/61
+% The first column in staID and the second is the index
+fid = fopen('station_index.txt');
+stationIndex = textscan(fid,'%s%s','CollectOutput',1);
+stationIndex = stationIndex{:};
+fid = fclose(fid);
+
+% index = ~cellfun(@isempty,strfind(cpraStationNames,'17StCanal'));
+
+% -------------------------------------------------------------------------
+
+% Need to get this in through some input information...
+% dtAdvisory = datenum(2012,8,27,9,0,0);
+dtAdvisory = datenum(forecastValid,'yyyymmddHHMMSS');
+
+% Loop through the number of ensemble simulations
+numEns = 1;
+ensFileNames = {'fort.61.imeds','fort.61.veerRight50.imeds'};
+for i = 1:numEns
+    % Read in ADCIRC time-series water levels
+    adcData(i) = readIMEDS(char(ensFileNames(i)));
+end
 
 sdate = round(adcData(1).STATION{1}.DATE(1));
 edate = round(adcData(1).STATION{1}.DATE(length(adcData(1).STATION{1}.DATE)));
@@ -61,26 +81,42 @@ ax = gca;
 fig = gcf;
 
 for f = 1:length(stations)
-       
-    % Get USACE water level data for station f
-    wl=rivergages2(stations{1,f},datestr(sdate-3),datestr(sdate),'HG');
     
-    % Plot USACE Observations
-    if isempty(wl) == 0
-        wl(:,1) = wl(:,1) + 5/24; % Adjust time from CDT to UTC
-    else
-        wl(1,1) = sdate; wl(1,2) = -20; % Create a fake point for legend purposes
+    % Find the cpraStation name for stations in stationIndex
+    f51Loc = find(~cellfun(@isempty,strfind(cpraStationNames,stationIndex(f))));
+       
+    % Get USACE water level data for station f51Loc
+    % If the rivergages function fails, then create a dummy value to plot.
+    try
+        %wl=rivergages2(stations{1,f},datestr(sdate-3),datestr(sdate),'HG');
+        wl=rivergages2(stations{1,f51Loc},datestr(sdate-3),datestr(sdate),'HG');
+        
+        % Plot USACE Observations
+        if isempty(wl) == 0
+            wl(:,1) = wl(:,1) + 5/24; % Adjust time from CDT to UTC
+        else
+            wl(1,1) = sdate; wl(1,2) = -20; % Create a fake point for legend purposes
+        end
+        % Plot USACE water levels as scatter points
+%         scatter(wl(:,1),wl(:,2),50,'MarkerEdgeColor','black','Linewidth',1);hold on;
+    catch ME
+        % USACE is down... :(
+        % Create some dummy values so the legned can still be plotted
+        wl(1,1) = sdate;
+        wl(1,2) = -20;
     end
+%         wl(1,1) = sdate;
+%         wl(1,2) = -20;
     % Plot USACE water levels as scatter points
     scatter(wl(:,1),wl(:,2),50,'MarkerEdgeColor','black','Linewidth',1);hold on;
-    
+     
 % -------------------------------------------------------------------------
     
     % Get ADCIRC data from station f and loop through each available ensemble
     for i = 1:numEns
-        adcData(i).STATION{f}.DATA(adcData(i).STATION{f}.DATA < -999) = NaN;
+        adcData(i).STATION{f51Loc}.DATA(adcData(i).STATION{f51Loc}.DATA < -999) = NaN;
         % Find min/max water surface elevation from ADCIRC result
-        res = ~any(~isnan(adcData(i).STATION{f}.DATA(:)));
+        res = ~any(~isnan(adcData(i).STATION{f51Loc}.DATA(:)));
         if res == false
     %         maxWL = ceil(max(adcData.STATION{f}.DATA / 0.3048)) + 3;
     %         minWL = floor(min(adcData.STATION{f}.DATA / 0.3048)) - 2;
@@ -91,10 +127,10 @@ for f = 1:length(stations)
             minWL = -4;
         else
             % Create some dummy values so the legned can still be plotted
-            adcData(i).STATION{f}.DATE(1) = sdate;
-            adcData(i).STATION{f}.DATE(1) = sdate+0.01;
-            adcData(i).STATION{f}.DATA(1) = -20;
-            adcData(i).STATION{f}.DATA(2) = -20;
+            adcData(i).STATION{f51Loc}.DATE(1) = sdate;
+            adcData(i).STATION{f51Loc}.DATE(2) = sdate+0.01;
+            adcData(i).STATION{f51Loc}.DATA(1) = -20;
+            adcData(i).STATION{f51Loc}.DATA(2) = -20;
 
             % Force max and min water level bounds
             maxWL = 18;
@@ -102,7 +138,8 @@ for f = 1:length(stations)
         end
     
         % Plot ADCIRC Simulation
-        plot(adcData(i).STATION{f}.DATE,adcData(i).STATION{f}.DATA / 0.3048,...
+        plot(adcData(i).STATION{f51Loc}.DATE,...
+            adcData(i).STATION{f51Loc}.DATA / 0.3048,...
             'Linewidth',3,'color',colors(i,:))
 
     end
@@ -141,14 +178,15 @@ for f = 1:length(stations)
     
 %     title1 = 'Storm:Isaac  -  grid:LA_v17a-WithUpperAtch_chk';
     title1 = strcat('Storm:',storm,' - grid:',adcGrid);
-    title2 = strcat(adcNames(f),'  -  USACE Gage ID ',stations(f));
+    title2 = strcat(cpraStationNames(f51Loc),'  -  USACE Gage ID ',stations(f51Loc));
     
     text(0,1.07,title1,'Units','normalized','Interpreter','None');
     text(0,1.03,title2,'Units','normalized','Interpreter','None');
     
     % Add Legend
-    legend('USACE Observations','nhcConsensus','veerRight50',...
-        'Location','northwest');
+%     legend('USACE Observations','nhcConsensus','veerRight50',...
+%         'Location','northwest');
+    legend('USACE Observations','nhcConsensus','Location','northwest');
     
     % Override some defaults
     set(gca,'LineWidth',1,'TickLength',[0.015 0.015]);
@@ -159,9 +197,7 @@ for f = 1:length(stations)
     % Plot Figure
     fig.PaperUnits = 'inches';
     fig.PaperPosition = [0 0 12.5 5];
-    fname = char(strcat(num2str(f),'_WSE_',stations(f)));
+    % Image filename is 'WSE_cpraStationID_USACEStationID'
+    fname = char(strcat('WSE_',stationIndex(f),'_USACE',stations(f51Loc)));
     print(fname,'-dpng','-r200');
 end
-
-
-
