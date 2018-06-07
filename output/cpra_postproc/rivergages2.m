f077a40c374c855dd9b58b67106780b35281070b
Matthew Bilskie <mbilsk3@lsu.edu>
2018-06-07 10:29:47 -0500

    Adding modules for hatteras. Fixed bug in rivergages2.m for displaing start and end dates.

--- output/cpra_postproc/rivergages2.m
+++ output/cpra_postproc/rivergages2.m
@@ -2,6 +2,9 @@
 % Max Agnew Maxwell.E.Agnew@usace.army.mil
 % Might have bugs, be sure to check variable name is appropriate
 % Writes our xml to temporary file then deletes, maybe there is better way?
+
+% Modified by M.V. Bilskie - 05-21-2018
+
 % example
 % rivergages('01280','2016-05-15 3:00','2016-06-15 3:00','HG')
 % rivergages('01280',datestr(now-15),datestr(now+1),'HG') %15days back
@@ -16,8 +19,6 @@ function [TW]=rivergages2(station,begindate,enddate,variable)
 % enddate='2018-04-10 0:00' %or datestr(now)
 % variable='HG'
 
-
-
 %DD=linspace(round(datenum(begindate)),round(datenum(enddate)),round((datenum(enddate)-datenum(begindate))/25));
 DD=linspace(round(datenum(begindate)),round(datenum(enddate)),round((datenum(enddate)-datenum(begindate))));
 
@@ -26,9 +27,6 @@ for g = 2:length(DD)
 
     bd=[datestr(DD(g-1),'yyyy-mm-dd'),'T',datestr(DD(g-1),'hh:MM')];
     ed=[datestr(DD(g),'yyyy-mm-dd'),'T',datestr(DD(g),'hh:MM')];
-    
-    print bd
-    print ed
 
     urlwrite(['http://rivergages.mvr.usace.army.mil/watercontrol/webservices/rest/webserviceWaterML.cfc?meth=getValues&site=',station,'&location=',station,'&variable=',variable,'&beginDate=',bd,'&endDate=',ed,'&authToken=RiverGages&method=RGWML'],'Gtest.xml');
     %urlwrite(['http://rivergages.mvr.usace.army.mil/watercontrol/webservices/rest/webserviceWaterML.cfm?meth=getSiteInfo&site=',station,'&authToken=RiverGages'],'Gtestsiteinfo.xml');
@@ -39,7 +37,7 @@ for g = 2:length(DD)
     %disp('Reading rivergages data:');
     %disp(B.sitesResponse.site.siteInfo.siteName.Text);
 
-    %delete('Gtest.xml')
+    delete('Gtest.xml')
     %delete('Gtestsiteinfo.xml')
     if isfield(A.timeSeriesResponse.timeSeries.values,'value')==1
         for f =1:length(A.timeSeriesResponse.timeSeries.values.value)
@@ -51,6 +49,6 @@ for g = 2:length(DD)
         end
         %else
             %disp('no data available')
-    end
+        end
 end
-end %function
+end
