6e12a3a9d437016a9a063cec9f7a54ae32364ccd
jasonfleming <jason.fleming@seahorsecoastal.com>
2018-08-12 08:27:10 -0400

    Corrected configuration files.

--- config/2018/asgs_config_nam_hatteras_texasr35h.sh
+++ config/2018/asgs_config_nam_hatteras_texasr35h.sh
@@ -28,7 +28,7 @@
 # Fundamental
 
 INSTANCENAME=readytx      # "name" of this ASGS process
-COLDSTARTDATE=2018080900  # calendar year month day hour YYYYMMDDHH24
+COLDSTARTDATE=2018070900  # calendar year month day hour YYYYMMDDHH24
 HOTORCOLD=coldstart       # "hotstart" or "coldstart"
 LASTSUBDIR=null           # path to previous execution (if HOTORCOLD=hotstart)
 HINDCASTLENGTH=30.0       # length of initial hindcast, from cold (days)
