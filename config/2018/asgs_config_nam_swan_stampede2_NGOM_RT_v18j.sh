5e0d8264e91bbb7985b4eec33c2bf28b6f11bd5d
Matthew V Bilskie <mbilsk3@lsu.edu>
2018-08-14 08:17:00 -0500

    Adding NAM SWAN NGOM_RT config file for stampede2

--- config/2018/asgs_config_nam_swan_stampede2_NGOM_RT_v18j.sh
+++ config/2018/asgs_config_nam_swan_stampede2_NGOM_RT_v18j.sh
@@ -47,7 +47,7 @@ PERL5LIB=${SCRIPTDIR}/PERL    # DateCale.pm perl module
 BACKGROUNDMET=on     # NAM download/forcing
 TIDEFAC=on           # tide factor recalc
 TROPICALCYCLONE=off  # tropical cyclone forcing
-WAVES=off            # wave forcing
+WAVES=on             # wave forcing
 VARFLUX=off          # variable river flux forcing
 
 # Computational Resources
