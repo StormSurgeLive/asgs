a00220447fc8e3f7eac3f2363147468b74658f6f
Jason Fleming <jason.fleming@seahorsecoastal.com>
2018-09-12 14:25:48 -0500

    Increased water level offset to 1ft (actually 30.48cm) for HSOFS and NC v9.99rivers mesh on lonestar5.

--- config/2018/asgs_config_062018_swan_river_lonestar_nc9.99wrivers.sh
+++ config/2018/asgs_config_062018_swan_river_lonestar_nc9.99wrivers.sh
@@ -164,7 +164,7 @@ INITPOST=null_init_post.sh
 POSTPROCESS=cera_post.sh
 
 # opendap
-TDS=(tacc_tds renci_tds)
+TDS=(tacc_tds lsu_tds renci_tds)
 TARGET=lonestar  # used in post processing to pick up HPC platform config
 # You must first have your ssh public key in ~/.ssh/authorized_keys2 file 
 # on the opendap server machine in order to scp files there via
