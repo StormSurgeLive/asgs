a26f731da949203b4bedf7d5f2a575be79c9ac36
Jason Fleming <jason.fleming@seahorsecoastal.com>
2019-03-20 13:20:48 -0500

    Added connection troubleshooting script for thredds servers.

---
+++ util/troubleshooting/test_scp_fortytwo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+while [ 1 ] ; do 
+   DATETIME=`date +'%Y-%h-%d-T%H:%M:%S%z'` 
+   echo "[${DATETIME}] INFO: Copying index-at.xml to fortytwo.cct.lsu.edu:/data/opendap" >> test_scp_fortytwo.log 
+   scp -vvv -P 2525 index-at.xml fortytwo.cct.lsu.edu:/data/opendap >> test_scp_fortytwo.log 2>&1
+   DATETIME=`date +'%Y-%h-%d-T%H:%M:%S%z'` 
+   echo "[${DATETIME}] INFO: Finished." >> test_scp_fortytwo.log 
+   sleep 10 
+done
