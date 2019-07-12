046a45f816bfd81333fb7c15a95f2aade65e4af5
Jason Fleming <jason.fleming@seahorsecoastal.com>
2019-07-12 14:33:26 -0500

    Added max wind speed +15% scenario.

--- config/2019/asgs_config_022019_queenbee_LAv19k.sh
+++ config/2019/asgs_config_022019_queenbee_LAv19k.sh
@@ -245,12 +245,12 @@ case $si in
    POSTPROCESS=null_post.sh
    ;;
 3)
-   ENSTORM=veerRight50
-   PERCENT=50
+   ENSTORM=veerRight100
+   PERCENT=100
    ;;
 2)
-   ENSTORM=veerRight50Wind10m
-   PERCENT=50
+   ENSTORM=veerRight100Wind10m
+   PERCENT=100
    ADCPREPWALLTIME="00:20:00"  # adcprep wall clock time, including partmesh
    FORECASTWALLTIME="00:20:00" # forecast wall clock time
    CONTROLTEMPLATE=LA_v19k-WithUpperAtch.nowindreduction.15.template
@@ -280,12 +280,12 @@ case $si in
    POSTPROCESS=null_post.sh
    ;;
 5)
-   ENSTORM=veerRight25
-   PERCENT=25
+   ENSTORM=veerLeft100
+   PERCENT=-100
    ;;
 4)
-   ENSTORM=veerRight25Wind10m
-   PERCENT=25
+   ENSTORM=veerLeft100Wind10m
+   PERCENT=-100
    ADCPREPWALLTIME="00:20:00"  # adcprep wall clock time, including partmesh
    FORECASTWALLTIME="00:20:00" # forecast wall clock time
    CONTROLTEMPLATE=LA_v19k-WithUpperAtch.nowindreduction.15.template
@@ -315,12 +315,12 @@ case $si in
    POSTPROCESS=null_post.sh
    ;;
 7)
-   ENSTORM=veerLeft100
-   PERCENT=-100
+   ENSTORM=maxWindSpeed15
+   PERCENT=15
    ;;
 6)
-   ENSTORM=veerLeft100Wind10m
-   PERCENT=-100
+   ENSTORM=maxWindSpeed15Wind10m
+   PERCENT=15
    ADCPREPWALLTIME="00:20:00"  # adcprep wall clock time, including partmesh
    FORECASTWALLTIME="00:20:00" # forecast wall clock time
    CONTROLTEMPLATE=LA_v19k-WithUpperAtch.nowindreduction.15.template
