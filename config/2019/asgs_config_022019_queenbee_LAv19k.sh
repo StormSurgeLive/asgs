41d953c103f1e1846b4e7797e9ece4f0543de368
Jason Fleming <jason.fleming@seahorsecoastal.com>
2019-07-12 09:18:54 -0500

    Added veer right 25% and removed veer right 100%.

--- config/2019/asgs_config_022019_queenbee_LAv19k.sh
+++ config/2019/asgs_config_022019_queenbee_LAv19k.sh
@@ -280,12 +280,12 @@ case $si in
    POSTPROCESS=null_post.sh
    ;;
 5)
-   ENSTORM=veerRight100
-   PERCENT=100
+   ENSTORM=veerRight25
+   PERCENT=25
    ;;
 4)
-   ENSTORM=veerRight100Wind10m
-   PERCENT=100
+   ENSTORM=veerRight25Wind10m
+   PERCENT=25
    ADCPREPWALLTIME="00:20:00"  # adcprep wall clock time, including partmesh
    FORECASTWALLTIME="00:20:00" # forecast wall clock time
    CONTROLTEMPLATE=LA_v19k-WithUpperAtch.nowindreduction.15.template
