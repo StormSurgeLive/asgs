9c91554e056a33311b4b035125bb7eca493131c8
Jason Fleming <jason.fleming@seahorsecoastal.com>
2019-07-24 11:35:06 -0500

    Updated daily runs on Texas mesh to use SWAN coupling.

--- config/2019/asgs_config_nam_stampede2_texas2008r35h.sh
+++ config/2019/asgs_config_nam_stampede2_texas2008r35h.sh
@@ -47,7 +47,7 @@ OUTPUTDIR=${SCRIPTDIR}/output # post processing scripts
 BACKGROUNDMET=on      # NAM download/forcing
 TIDEFAC=on            # tide factor recalc
 TROPICALCYCLONE=off   # tropical cyclone forcing
-WAVES=off              # wave forcing
+WAVES=on              # wave forcing
 VARFLUX=off           # variable river flux forcing
 
 # Computational Resources
