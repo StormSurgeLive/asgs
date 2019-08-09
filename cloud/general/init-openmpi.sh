98b81411d2c75346d551945acf6fc741e5dcd10e
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2019-08-09 07:37:53 -0500

    Created a Perl script to manage building all Fortran utilities.
    Issue 107: Built an extensible Perl script to manage the building
    of an entire ASGS environment locally. Currently the steps in the
    script focus on the building of NetCDF, HDF5, and any Fortran
    utilities in the ASGS repository that have a makefile associated
    with them. Extending this script to handle most, if not all of the
    tasks associated with building a working ASGS environment from
    scratch on any platform should be broken out into future Issues.
    The groundwork has been laid with this script. A basic unit test
    was created, but it's not sufficient to exercise the script. Some
    changes were made to existing makefiles and scripts that are used
    to do the tedious work associate with building and compiling.

--- cloud/general/init-openmpi.sh
+++ cloud/general/init-openmpi.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/bash
 
-TMP=$HOME/tmp 2> /dev/null
+TMP=$HOME/tmp
+OPT=${1-$HOME/opt}
+COMPILER=${2-intel}
+
+OPENMPI_VERSION=openmpi-1.8.1
+
+if [ $2 == "clean" ]; then 
+  echo Cleaning OpenMPI libraries and utilities
+  rm -rfv $OPT/$OPENMPI_VERSION-$COMPILER
+  exit
+fi
+
+if [ $COMPILER == "intel" ]; then 
+  export CC=icc
+  export FC=ifort
+  export CXX=icpc
+fi
+if [ $COMPILER == "gfortran" ]; then 
+  export CC=gcc
+  export FC=gfortran
+  export CXX=g++
+fi
+
 mkdir -p $TMP
-PREFIX=$HOME/opt/openmpi-1.8.1 2> /dev/null
-mkdir -p $PREFIX
+mkdir -p $OPT/$OPENMPI_VERSION-$COMPILER
 cd $TMP
-wget https://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.1.tar.gz
-tar -xvf openmpi-1.8.1.tar.gz
-cd openmpi-1.8.1
-./configure --prefix=$PREFIX
+
+wget https://www.open-mpi.org/software/ompi/v1.8/downloads/${OPENMPI_VERSION}.tar.gz
+tar -xvf $OPENMPI_VERSION.tar.gz
+cd $OPENMPI_VERSION 
+
+./configure --prefix=$OPT
 make
 make install
--- install/install-hdf5-netcdf4.sh
+++ install/install-hdf5-netcdf4.sh
@@ -1,25 +1,41 @@
-#!/bin/sh
-
-export CC=icc
-export FC=ifort
-export CXX=icpc
-#export CPP=
+#!/bin/bash
 
 TMP=$HOME/tmp
-OPT=$HOME/opt/usr
+OPT=${1-$HOME/opt}
+COMPILER=${2-intel}
 
-export LD_LIBRARY_PATH=$OPT/lib:$LD_LIBRARY_PATH
-export LD_INCLUDE_PATH=$OPT/lib:$LD_INCLUDE_PATH
-export CPPFLAGS=-I$OPT/include
-export LDFLAGS=-L$OPT/lib
+if [ $2 == "clean" ]; then 
+  echo Cleaning HDF5 and NetCDF libraries and utilities
+  cd $OPT/bin
+  rm -fv gif2h5 h5cc h5debug h5dump h5import h5ls h5perf_serial h5repack h5stat nc-config ncdump ncgen3 h52gif h5copy h5diff h5fc h5jam h5mkgrp h5redeploy h5repart h5unjam nccopy ncgen nf-config
+  cd $OPT/lib
+  rm -fv libhd5* libhdf5 libnetcdf*
+  cd $OPT/include
+  rm -fv netcdf* hd5* HD5* hdf*
+  rm -rvf $TMP/hdf5-1.8.12*
+  rm -rvf $TMP/netcdf-4.2.1.1*
+  rm -rvf $TMP//netcdf-fortran-4.2*
+  exit
+fi
+
+if [ $COMPILER == "intel" ]; then 
+  export CC=icc
+  export FC=ifort
+  export CXX=icpc
+fi
+if [ $COMPILER == "gfortran" ]; then 
+  export CC=gcc
+  export FC=gfortran
+  export CXX=g++
+fi
 
 mkdir -p $TMP
 cd $TMP
 
 if [ ! -e hdf5-1.8.12.tar.gz ]; then
-  wget https://asgs-static-assets.sfo2.digitaloceanspaces.com/lib/hdf5-1.8.12.tar.gz
-  wget https://asgs-static-assets.sfo2.digitaloceanspaces.com/lib/netcdf-4.2.1.1.tar.gz
-  wget https://asgs-static-assets.sfo2.digitaloceanspaces.com/lib/netcdf-fortran-4.2.tar.gz
+  wget --verbose https://asgs-static-assets.sfo2.digitaloceanspaces.com/lib/hdf5-1.8.12.tar.gz
+  wget --verbose https://asgs-static-assets.sfo2.digitaloceanspaces.com/lib/netcdf-4.2.1.1.tar.gz
+  wget --verbose https://asgs-static-assets.sfo2.digitaloceanspaces.com/lib/netcdf-fortran-4.2.tar.gz
 fi
 
 mkdir -p $OPT 2> /dev/null
@@ -27,6 +43,7 @@ mkdir -p $OPT 2> /dev/null
 if [ ! -e $OPT/bin/h5diff ]; then
   tar zxvf hdf5-1.8.12.tar.gz
   cd hdf5-1.8.12
+  make clean
   ./configure --prefix $OPT --enable-fortran
   make -j4 install
   cd ../
@@ -40,6 +57,7 @@ fi
 if [ ! -e $OPT/bin/nc-config ]; then
   tar zxvf netcdf-4.2.1.1.tar.gz
   cd netcdf-4.2.1.1
+  make clean
   ./configure --prefix $OPT
   make -j4 install
   cd ..
@@ -53,6 +71,7 @@ fi
 if [  ! -e $OPT/bin/nf-config ] && [ -z "$($OPT/bin/nf-config --all  | grep '\-\-has\-f90   \-> yes')" ]; then
   tar zxvf netcdf-fortran-4.2.tar.gz
   cd netcdf-fortran-4.2
+  make clean
   ./configure --prefix $OPT
   make -j4 install
   cd ..
