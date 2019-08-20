d17097188d4a10b1a55a22aade3a2980bc5780cb
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2019-08-19 23:45:37 -0500

    Improvements to asgs-brew.pl and workings on vagrant/stampede2.
    Issue 107: Second commit for this issue, added options to asgs-brew.pl,
    made sure it built an environment that was suitable for the ultimte
    building of adcirc and swat (adcirc-cg), added documentation, a debug
    flag, and a flag to manage the number of jobs `make` used.

--- cloud/general/init-adcirc.sh
+++ cloud/general/init-adcirc.sh
@@ -1,8 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 if [ ! -d $HOME/adcirc-cg/work ]; then
+  cd $HOME
   git clone https://github.com/adcirc/adcirc-cg.git
 fi
+
+export PERLBREW_PERL=perl-5.28.2
+export PERLBREW_ROOT=/home/vagrant/perl5/perlbrew
+export PERLBREW_PATH=/home/vagrant/perl5/perlbrew/bin:/home/vagrant/perl5/perlbrew/perls/perl-5.28.2/bin
+export PERLBREW_HOME=/home/vagrant/.perlbrew
+export NETCDFHOME=/home/vagrant/opt
+export LD_INCLUDE_PATH=/home/vagrant/opt/include
+export PATH=/home/vagrant/perl5/perlbrew/perls/perl-5.28.2/bin:/home/vagrant/opt/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export LD_LIBRARY_PATH=/home/vagrant/opt/lib
+export PERLBREW_MANPATH=/home/vagrant/perl5/perlbrew/perls/perl-5.28.2/man
+
 cd $HOME/adcirc-cg/work
-export PATH=$PATH:$HOME/opt/openmpi-1.8.1/bin
-make all compiler=gfortran NETCDF=enable NETCDF4=enable NETCDF4_COMPRESSION=enable MACHINENAME=jason-desktop
+make clean && make all compiler=gfortran NETCDF=enable NETCDF4=enable NETCDF4_COMPRESSION=enable NETCDFHOME=$NETCDFHOME MACHINENAME=jason-desktop
