b0ce378a554e8f43b7d8b64b934cc7b6fcc6da33
Jason Fleming <jason.fleming@seahorsecoastal.com>
2019-08-30 20:23:03 -0500

    Increased number of cores to 1600 on supermic.

--- config/2019/asgs_config_hsofs_al052019_jgf.sh
+++ config/2019/asgs_config_hsofs_al052019_jgf.sh
@@ -44,14 +44,14 @@ TROPICALCYCLONE=on       # tropical cyclone forcing
    STORM=05              # storm number, e.g. 05=ernesto in 2006
    YEAR=2019             # year of the storm
 WAVES=on                 # wave forcing
-   REINITIALIZESWAN=no   # used to bounce the wave solution
+   REINITIALIZESWAN=yes   # used to bounce the wave solution
 VARFLUX=off               # variable river flux forcing
 CYCLETIMELIMIT="99:00:00"
 
 # Computational Resources (related defaults set in platforms.sh)
 
-NCPU=959                     # number of compute CPUs for all simulations
-NCPUCAPACITY=3648
+NCPU=1799                     # number of compute CPUs for all simulations
+NCPUCAPACITY=9999
 NUMWRITERS=1
 ACCOUNT=null
 if [[ $HPCENVSHORT = "hatteras" ]]; then
