23c912b27e2160f975970a3133d602822e2eee70
Jason Fleming <jason.fleming@seahorsecoastal.com>
2019-09-08 08:04:56 -0500

    Reduced al052019 scenario package to just the OFCL trakc; changed southfl nam intendedaudience to general.

--- config/2019/asgs_config_hsofs_al052019_jgf.sh
+++ config/2019/asgs_config_hsofs_al052019_jgf.sh
@@ -82,7 +82,7 @@ LASTSUBDIR=http://fortytwo.cct.lsu.edu:8080/thredds/fileServer/2019/al05/30/hsof
 # Scenario package
 
 #PERCENT=default
-SCENARIOPACKAGESIZE=6
+SCENARIOPACKAGESIZE=2
 if [[ $HPCENVSHORT = "hatteras" ]]; then
    if [[ $USER = "jgflemin" || $USER = "ncfs" ]]; then
       SCENARIOPACKAGESIZE=2
