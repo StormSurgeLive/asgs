7b41c46577db733b9435c619d31f801bc26461e2
Jason Fleming <jason.fleming@seahorsecoastal.com>
2019-09-13 08:39:01 -0500

    Latest config files for al092019 running on supermic.

--- config/2019/asgs_config_neflga_al092019_jgf.sh
+++ config/2019/asgs_config_neflga_al092019_jgf.sh
@@ -67,12 +67,11 @@ OPENDAPNOTIFY="asgs.cera.lsu@gmail.com,jason.g.fleming@gmail.com"
 
 COLDSTARTDATE=auto   # calendar year month day hour YYYYMMDDHH24
 HOTORCOLD=hotstart   # "hotstart" or "coldstart" ; only used for initial run
-LASTSUBDIR=/work/jgflemin/asgs14074/initialize
-
+LASTSUBDIR=http://fortytwo.cct.lsu.edu:8080/thredds/fileServer/2019/al09/02/neflga_v12_geo/queenbee.loni.org/neflga_al092019_jgf/nhcConsensus
 # Scenario package
 
 #PERCENT=default
-SCENARIOPACKAGESIZE=4
+SCENARIOPACKAGESIZE=2
 case $si in
    -2) 
        ENSTORM=hindcast
