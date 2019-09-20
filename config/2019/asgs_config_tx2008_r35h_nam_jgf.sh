2473117b8edb369c2cc349147bd2b30cdb0d94e4
Jason Fleming <jason.fleming@seahorsecoastal.com>
2019-09-20 14:30:09 -0500

    Reduced Texas forecasts to 1/day after Imelda impacts.

--- config/2019/asgs_config_tx2008_r35h_nam_jgf.sh
+++ config/2019/asgs_config_tx2008_r35h_nam_jgf.sh
@@ -39,7 +39,7 @@ source $SCRIPTDIR/config/mesh_defaults.sh
 TIDEFAC=on               # tide factor recalc
    HINDCASTLENGTH=30.0   # length of initial hindcast, from cold (days)
 BACKGROUNDMET=on         # NAM download/forcing
-   FORECASTCYCLE="00,06,12,18"
+   FORECASTCYCLE="06"
 TROPICALCYCLONE=off      # tropical cyclone forcing
    STORM=05              # storm number, e.g. 05=ernesto in 2006
    YEAR=2019             # year of the storm
