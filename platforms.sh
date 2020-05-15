3ebcafd9a75034fe4a4c0eed789917d386643e28
wwlwpd <6434714+wwlwpd@users.noreply.github.com>
2020-05-14 21:42:17 -0500

    Fixed default profile name if not specified.
    Issue #227: If the default profile name was not specified, the
    process Id of the init-asgs.sh script was used instead of the
    string "default" which is presented as what's set if not specified.
    
    Fixed a similar issue with the default --install-path.
    
    Found 2 missing Perl modules, added them:
    
    * XML::RSS (for ./get_atcf.pl)
    * Template (for advisory replay)

--- platforms.sh
+++ platforms.sh
@@ -494,12 +494,11 @@ init_stampede2()
   #
   RMQMessaging_LocationName="TACC"
   RMQMessaging_ClusterName="Stampede2"
-  RMQMessaging_Enable="on"      # "on"|"off"
-  RMQMessaging_Transmit="on"    #  enables message transmission ("on" | "off")
+  RMQMessaging_Enable="on"              # "on"|"off"
+  RMQMessaging_Transmit="on"            #  enables message transmission ("on" | "off")
   RMQMessaging_NcoHome="$WORK/local"
-  RMQMessaging_Python=/opt/apps/intel18/python2/2.7.15/bin/python
-  #
-  PLATFORMMODULES='module load intel/18.0.2 python2/2.7.15 xalt/2.6.5 TACC'
+  RMQMessaging_Python=python            # should pick up whatever is in PATH
+  PLATFORMMODULES='module load intel/18.0.2 xalt/2.6.5 TACC' # removed: 'python2/2.7.15'
   SERIALMODULES='module load' # no extra modules for serial jobs
   PARALLELMODULES='module load libfabric/1.7.0 impi/18.0.2'
   # matlab
