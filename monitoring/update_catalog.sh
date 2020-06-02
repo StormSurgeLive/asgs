ab00b4f1eb2ce58b558100de1361e911806e0f36
jasonfleming <jason.fleming@seahorsecoastal.com>
2020-06-02 14:46:03 -0400

    Added bblanton's catalog monitoring script.

---
+++ monitoring/update_catalog.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#set -o verbose
+#set -o xtrace 
+
+# Definitions and paths
+MakeCatalog=1
+
+DHOME="/projects/ncfs/opendap/data/2019"
+
+###functions
+filesec()
+{
+   perl -e '$epoch = (stat("$ARGV[0]"))[9]; print "$epoch";'  "$1"
+}
+
+filetime()
+{
+   perl -e '$tt = print scalar(localtime($ARGV[0])),qq(\n);' "$1"
+}
+now()
+{
+	date --utc +%s
+}
+
+cd $DHOME
+
+echo ' '
+
+if [ "$MakeCatalog" == "1" ]  ; then 
+	if [ -e "catalog.tree" ] ; then
+		mv catalog.tree catalog.tree.old
+	fi
+	sh makecat.sh  > catalog.tree
+
+    # split catalog into nam and tc parts, for convenience
+    sed -n '1,3p' catalog.tree > header
+    sed '/nam/d' catalog.tree > catalog.tree.tc
+
+    sed -n '/nc_/p' catalog.tree.tc > temp 
+    cat temp >> header
+    mv header catalog.tree.tc-nc
+
+    sed -n '/nam/p' catalog.tree > temp 
+    cat temp >> header
+    mv header catalog.tree.nam
+    rm temp
+
+else
+	echo Skipping catalog generation/update
+fi
+
+# update ncml in "current" directories
+
+#python $DHOME/make_ncml.py /projects/ncfs/opendap/data/2019/NCFS_CURRENT_TROPICAL /projects/ncfs/opendap/data/SSV-Ncml/ RenciAsgs_LatestTropical.ncml LatestTropical
+
+#python $DHOME/make_ncml.py /projects/ncfs/opendap/data/NCFS_CURRENT_DAILY /projects/ncfs/opendap/data/SSV-Ncml/  RenciAsgs_LatestDaily.ncml  LatestDaily
+
+
+
