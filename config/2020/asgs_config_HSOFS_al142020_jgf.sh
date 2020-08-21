fac84535c55a3958da2f6fb47fb7d2fbb49bbea5
Jason Fleming <jason.fleming@seahorsecoastal.com>
2020-08-21 12:55:00 -0500

    Updated NEFLGAv12b to just run nowcasts; added config for HSOFS on ls5 for al142020 td 14.

--- config/2020/asgs_config_HSOFS_al142020_jgf.sh
+++ config/2020/asgs_config_HSOFS_al142020_jgf.sh
@@ -36,23 +36,23 @@
 
 # Fundamental
 
-INSTANCENAME=NEFLGAv12b_nam_jgf  # "name" of this ASGS process
+INSTANCENAME=HSOFS_al142020_jgf  # "name" of this ASGS process
 
 # Input files and templates
 
-GRIDNAME=NEFLGAv12b
+GRIDNAME=HSOFS
 source $SCRIPTDIR/config/mesh_defaults.sh
 
 # Physical forcing (defaults set in config/forcing_defaults)
 
 TIDEFAC=on            # tide factor recalc
 HINDCASTLENGTH=30.0   # length of initial hindcast, from cold (days)
-BACKGROUNDMET=on      # NAM download/forcing
+BACKGROUNDMET=off     # NAM download/forcing
 FORECASTCYCLE="06"
-TROPICALCYCLONE=off   # tropical cyclone forcing
-#STORM=07             # storm number, e.g. 05=ernesto in 2006
-#YEAR=2018            # year of the storm
-WAVES=off              # wave forcing
+TROPICALCYCLONE=on    # tropical cyclone forcing
+STORM=14              # storm number, e.g. 05=ernesto in 2006
+YEAR=2020             # year of the storm
+WAVES=on              # wave forcing
 #STATICOFFSET=0.1524
 REINITIALIZESWAN=no   # used to bounce the wave solution
 VARFLUX=off           # variable river flux forcing
@@ -60,7 +60,7 @@ CYCLETIMELIMIT="99:00:00"
 
 # Computational Resources (related defaults set in platforms.sh)
 
-NCPU=511                     # number of compute CPUs for all simulations
+NCPU=1000                     # number of compute CPUs for all simulations
 NUMWRITERS=1
 NCPUCAPACITY=9999
 #
@@ -90,12 +90,12 @@ TDS=( tacc_tds )
 
 COLDSTARTDATE=auto
 HOTORCOLD=hotstart       # "hotstart" or "coldstart"
-LASTSUBDIR=http://tds.renci.org:8080/thredds/fileServer/2020/nam/2020081706/NEFLGAv12b/hatteras.renci.org/NEFLGAv12b_nam_jgf/namforecast
+LASTSUBDIR=http://tds.renci.org:8080/thredds/fileServer/2020/nam/2020082006/hsofs/hatteras.renci.org/hsofs-nam-bob/namforecast
 
 # Scenario package 
 
 #PERCENT=default
-SCENARIOPACKAGESIZE=0
+SCENARIOPACKAGESIZE=2
 case $si in
  -2)
    ENSTORM=hindcast
@@ -105,11 +105,11 @@ case $si in
    ENSTORM=nowcast
    ;;
  0)
-   ENSTORM=namforecastWind10m
+   ENSTORM=nhcConsensusWind10m
    source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on "Wind10m" suffix
    ;;
 1)
-   ENSTORM=namforecast
+   ENSTORM=nhcConsensus
    ;;
 *)
    echo "CONFIGRATION ERROR: Unknown scenario number: '$si'."
