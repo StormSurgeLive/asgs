2c891c1973b5a5d0a5550f678bb2b3ffb14df69c
Jason Fleming <jason.fleming@seahorsecoastal.com>
2020-08-22 04:23:09 -0500

    Updated config to not use priority on queenbeeC.

--- config/2020/asgs_config_LAv20a_al142020_jgf.sh
+++ config/2020/asgs_config_LAv20a_al142020_jgf.sh
@@ -71,8 +71,11 @@ NUMWRITERS=1
 NCPUCAPACITY=9999 
 ACCOUNT=loni_cera_2020
 PARTITION=null
-QUEUENAME=priority    # queenbee2 and supermic
-SERQUEUE=priority     # queenbee2 and supermic
+#QUEUENAME=priority    # queenbee2 and supermic
+#SERQUEUE=priority     # queenbee2 and supermic
+QUEUENAME=workq   # not priority
+SERQUEUE=single   # not priority
+
 #QOS=vip               # stampede2 and lonestar5
 #
 if [[ $USER = jgflemin ]]; then
@@ -124,24 +127,28 @@ case $si in
    ENSTORM=nowcast
    ;;
  0)
-   ENSTORM=nhcConsensusWind10m
+   ENSTORM=veerRight100Wind10m
+   PERCENT=100
    source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on "Wind10m" suffix
    QUEUENAME=workq   # not priority
    SERQUEUE=single   # not priority
    ;;
  1)
-   ENSTORM=nhcConsensus
+   ENSTORM=veerRight100
+   PERCENT=100
    QUEUENAME=workq   # not priority
    SERQUEUE=single   # not priority
    ;;
  2)
-   ENSTORM=veerRight100Wind10m
-   PERCENT=100
+   ENSTORM=nhcConsensusWind10m
    source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on "Wind10m" suffix
+   QUEUENAME=workq   # not priority
+   SERQUEUE=single   # not priority
    ;;
  3)
-   ENSTORM=veerRight100
-   PERCENT=100
+   ENSTORM=nhcConsensus
+   QUEUENAME=workq   # not priority
+   SERQUEUE=single   # not priority
    ;;
 *)
    echo "CONFIGRATION ERROR: Unknown scenario number: '$si'."
