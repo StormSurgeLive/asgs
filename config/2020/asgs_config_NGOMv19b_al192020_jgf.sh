26b206af1cc4ebc74ffce457dd75f6c1f042e117
Jason Fleming <jason.fleming@seahorsecoastal.com>
2020-09-13 10:22:21 -0500

    Updated boundary conditions and added support for queenbeeC to NGOMv19b mesh.

--- config/2020/asgs_config_NGOMv19b_al192020_jgf.sh
+++ config/2020/asgs_config_NGOMv19b_al192020_jgf.sh
@@ -36,7 +36,7 @@
 
 # Fundamental
 
-INSTANCENAME=NGOMv19b_nam_jgf  # "name" of this ASGS process
+INSTANCENAME=NGOMv19b_al192020_jgf  # "name" of this ASGS process
 
 # Input files and templates
 
@@ -44,18 +44,17 @@ GRIDNAME=NGOMv19b
 source $SCRIPTDIR/config/mesh_defaults.sh
 
 #jgf20200721 : new template file with Matt's boundary condition
-#CONTROLTEMPLATE=NGOM_RT_v19b.15.template_13kcms # <---<<< default is NGOM_RT_v19b.15.template_18kcms in $SCRIPTDIR/config/mesh_defaults.sh
 CONTROLTEMPLATE=NGOM_RT_v19b.15.template_10kcms # <---<<< default is NGOM_RT_v19b.15.template_18kcms in $SCRIPTDIR/config/mesh_defaults.sh
 
 # Physical forcing (defaults set in config/forcing_defaults)
 
 TIDEFAC=on            # tide factor recalc
 HINDCASTLENGTH=30.0   # length of initial hindcast, from cold (days)
-BACKGROUNDMET=on      # NAM download/forcing
-FORECASTCYCLE="00,06,12,18"
-TROPICALCYCLONE=off   # tropical cyclone forcing
-#STORM=07             # storm number, e.g. 05=ernesto in 2006
-#YEAR=2018            # year of the storm
+BACKGROUNDMET=off      # NAM download/forcing
+FORECASTCYCLE="06"
+TROPICALCYCLONE=on   # tropical cyclone forcing
+STORM=19              # storm number, e.g. 05=ernesto in 2006
+YEAR=2020             # year of the storm
 WAVES=off             # wave forcing
 #STATICOFFSET=0.1524
 REINITIALIZESWAN=no   # used to bounce the wave solution
@@ -64,9 +63,11 @@ CYCLETIMELIMIT="99:00:00"
 
 # Computational Resources (related defaults set in platforms.sh)
 
-NCPU=959               # number of compute CPUs for all simulations
+NCPU=479               # number of compute CPUs for all simulations
 NUMWRITERS=1
 NCPUCAPACITY=9999 
+ACCOUNT=loni_cera_2020
+PARTITION=null
 #QUEUENAME=priority    # queenbee2 and supermic
 #SERQUEUE=priority     # queenbee2 and supermic
 #QOS=vip               # stampede2 and lonestar5
@@ -80,8 +81,8 @@ if [[ $USER = jgflemin ]]; then
    if [[ $HPCENVSHORT = queenbeeC ]]; then
       ADCIRCDIR=/work/jgflemin/adcirc-cg-v53release-qbc-intel/work
       SWANDIR=/work/jgflemin/adcirc-cg-v53release-qbc-intel/swan
-      ACCOUNT=loni_cera_2020
       PARTITION=null
+      
    fi
    if [[ $HPCENVSHORT = frontera ]]; then
       ADCIRCDIR=$WORK/adcirc-cg/adcirc/v53release/work
@@ -110,14 +111,14 @@ fi
 
 # Initial state (overridden by STATEFILE after ASGS gets going)
 
-COLDSTARTDATE=2020080800
-HOTORCOLD=coldstart     # "hotstart" or "coldstart"
-LASTSUBDIR=null
+COLDSTARTDATE=auto
+HOTORCOLD=hotstart     # "hotstart" or "coldstart"
+LASTSUBDIR=https://fortytwo.cct.lsu.edu/thredds/fileServer/2020/nam/2020091200/NGOMv19b/supermic.hpc.lsu.edu/NGOMv19b_nam_jgf/namforecast
 
 # Scenario package 
 
 #PERCENT=default
-SCENARIOPACKAGESIZE=2 # number of storms in the ensemble
+SCENARIOPACKAGESIZE=4 # number of storms in the ensemble
 case $si in
  -2)
    ENSTORM=hindcast
@@ -126,12 +127,21 @@ case $si in
    # do nothing ... this is not a forecast
    ENSTORM=nowcast
    ;;
- 0)
-   ENSTORM=namforecastWind10m
+0)
+   ENSTORM=veerRight100Wind10m
+   PERCENT=100
    source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on "Wind10m" suffix
    ;;
 1)
-   ENSTORM=namforecast
+   ENSTORM=veerRight100
+   PERCENT=100
+   ;;
+2)
+   ENSTORM=nhcConsensusWind10m
+   source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on "Wind10m" suffix
+   ;;
+3)
+   ENSTORM=nhcConsensus
    ;;
 *)
    echo "CONFIGRATION ERROR: Unknown scenario number: '$si'."
@@ -140,4 +150,3 @@ esac
 
 PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz
 HINDCASTARCHIVE=prepped_${GRIDNAME}_hc_${INSTANCENAME}_${NCPU}.tar.gz
-
