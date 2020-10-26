ea7d70f5ca4317d8bd1fb917d383fbd1930732e3
Jason Fleming <jason.fleming@seahorsecoastal.com>
2020-10-26 08:44:50 -0500

    Added veerLeft100 to LAv20a for ts zeta al282020; added veerRight100 to NGOMv19b for ts zeta al282020.

--- config/2020/asgs_config_NGOMv19b_al282020_jgf.sh
+++ config/2020/asgs_config_NGOMv19b_al282020_jgf.sh
@@ -119,7 +119,7 @@ LASTSUBDIR=https://fortytwo.cct.lsu.edu/thredds/fileServer/2020/nam/2020102512/N
 # Scenario package 
 
 #PERCENT=default
-SCENARIOPACKAGESIZE=2 # number of storms in the ensemble
+SCENARIOPACKAGESIZE=4 # number of storms in the ensemble
 case $si in
  -2)
    ENSTORM=hindcast
@@ -134,6 +134,15 @@ case $si in
    ;;
 1)
    ENSTORM=nhcConsensus
+   ;;
+ 2)
+   ENSTORM=veerRight100Wind10m
+   source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on "Wind10m" suffix
+   PERCENT=100
+   ;;
+3)
+   ENSTORM=veerRight100
+   PERCENT=100
    ;;
 *)
    echo "CONFIGRATION ERROR: Unknown scenario number: '$si'."
