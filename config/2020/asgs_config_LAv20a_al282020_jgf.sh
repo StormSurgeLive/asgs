06a8499a61b74dada31656c22ff1298bf51f0908
Jason Fleming <jason.fleming@seahorsecoastal.com>
2020-10-29 15:30:58 -0500

    Reduced scenario package size to just the official forecast tracks.

--- config/2020/asgs_config_LAv20a_al282020_jgf.sh
+++ config/2020/asgs_config_LAv20a_al282020_jgf.sh
@@ -123,7 +123,7 @@ LASTSUBDIR=https://fortytwo.cct.lsu.edu/thredds/fileServer/2020/nam/2020102712/L
 # Scenario package 
 
 #PERCENT=default
-SCENARIOPACKAGESIZE=6 # number of storms in the ensemble
+SCENARIOPACKAGESIZE=2 # number of storms in the ensemble
 case $si in
  -2)
    ENSTORM=hindcast
