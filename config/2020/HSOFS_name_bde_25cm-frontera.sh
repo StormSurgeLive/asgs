7e78575bc2c63c866abf4cb4e80fda387bc19ba8
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2020-10-29 09:07:04 -0500

    adding config changes for Zeta AL282020

--- config/2020/HSOFS_name_bde_25cm-frontera.sh
+++ config/2020/HSOFS_name_bde_25cm-frontera.sh
@@ -8,7 +8,7 @@
 # etc)
 #-------------------------------------------------------------------
 #
-# Copyright(C) 2020 Jason Fleming
+# Copyright(C) 2018--2020 Jason Fleming
 #
 # This file is part of the ADCIRC Surge Guidance System (ASGS).
 #
@@ -24,9 +24,19 @@
 # You should have received a copy of the GNU General Public License along with
 # the ASGS.  If not, see <http://www.gnu.org/licenses/>.
 #-------------------------------------------------------------------
+# The defaults for parameters that can be reset in this config file 
+# are preset in the following scripts:
+# {SCRIPTDIR/platforms.sh               # also contains Operator-specific info
+# {SCRIPTDIR/config/config_defaults.sh
+# {SCRIPTDIR/config/mesh_defaults.sh
+# {SCRIPTDIR/config/forcing_defaults.sh
+# {SCRIPTDIR/config/io_defaults.sh
+# {SCRIPTDIR/config/operator_defaults.sh
+#-------------------------------------------------------------------
 
 # Fundamental
-INSTANCENAME=CTXCS2017_nam_bde_10cm     # "name" of this ASGS process
+
+INSTANCENAME=HSOFS_nam_bde_25cm  # "name" of this ASGS process
 ACCOUNT=ASC20001
 QOS=vippj_p3000 # for priority during a storm
 QUEUENAME=normal # same as SLURM partition
@@ -37,71 +47,64 @@ ASGSADMIN="asgsnotifications@opayq.com"
 
 # Input files and templates
 
-GRIDNAME=CTXCS2017
+GRIDNAME=HSOFS
 source $SCRIPTDIR/config/mesh_defaults.sh
-NAFILE=ctx_gr_p01E02_na_p02_10cm_fort.13
+NAFILE=hsofs_25cm.13
 NAPROPERTIES=${NAFILE}.properties
+CONTROLTEMPLATE=hsofs_explicit_25cm.15.template
+CONTROLPROPERTIES=${CONTROLTEMPLATE}.properties 
 
-# Physical forcing (defaults set in config/forcing_defaults.sh)
-TIDEFAC=on               # tide factor recalc
-   HINDCASTLENGTH=30.0   # length of initial hindcast, from cold (days)
-BACKGROUNDMET=on         # NAM download/forcing
-   FORECASTCYCLE="00,06,12,18"
-TROPICALCYCLONE=off      # tropical cyclone forcing
-   STORM=26              # storm number, e.g. 05=ernesto in 2006
-   YEAR=2020             # year of the storm
-WAVES=off                # wave forcing
-   REINITIALIZESWAN=no   # used to bounce the wave solution
-VARFLUX=off              # variable river flux forcing
-#STATICOFFSET=0.30
-#
+# Physical forcing (defaults set in config/forcing_defaults)
+TIDEFAC=on                  # tide factor recalc
+HINDCASTLENGTH=30.0         # length of initial hindcast, from cold (days)
+BACKGROUNDMET=on            # NAM download/forcing
+FORECASTCYCLE="00,06,12,18" # 
+TROPICALCYCLONE=off         # tropical cyclone forcing
+STORM=22                    # storm number, e.g. 05=ernesto in 2006
+YEAR=2020                   # year of the storm
+WAVES=on                    # wave forcing (on|off)
+REINITIALIZESWAN=no         # used to bounce the wave solution
+VARFLUX=off                 # variable river flux forcing
 CYCLETIMELIMIT="99:00:00"
 
 # Computational Resources (related defaults set in platforms.sh)
-
-NCPU=2015                    # number of compute CPUs for all simulations
-NCPUCAPACITY=9999
+NCPU=1919                    # number of compute CPUs for all simulations
 NUMWRITERS=1
+NCPUCAPACITY=99999 
 
-# Post processing and publication
-
-INTENDEDAUDIENCE=general    # "general" | "developers-only" | "professional"
-#POSTPROCESS=( accumulateMinMax.sh createMaxCSV.sh cpra_slide_deck_post.sh includeWind10m.sh createOPeNDAPFileList.sh opendap_post.sh )
+INTENDEDAUDIENCE=general    # can also be "developers-only" or "professional"
 POSTPROCESS=( includeWind10m.sh createOPeNDAPFileList.sh opendap_post.sh )
-OPENDAPNOTIFY="asgs.cera.lsu@gmail.com,jason.g.fleming@gmail.com,clint@oden.utexas.edu,amin.kiaghadi2013@gmail.com,m.botto_t@utexas.edu,asgsnotifications@opayq.com,rluettich1@gmail.com,cera.asgs.tk@gmail.com,asgsnotes4ian@gmail.com,Patrick.C.Kerr@usace.army.mil"
-NOTIFY_SCRIPT=ut-nam-notify.sh
-TDS=( tacc_tds lsu_tds )
+OPENDAPNOTIFY="asgs.cera.lsu@gmail.com,jason.g.fleming@gmail.com,rluettich1@gmail.com,cera.asgs.tk@gmail.com,asgsnotes4ian@gmail.com,asgsnotifications@opayq.com"
+TDS=( lsu_tds tacc_tds )
 
 # Initial state (overridden by STATEFILE after ASGS gets going)
+COLDSTARTDATE=auto #2020092000 
+HOTORCOLD=hotstart    # "hotstart" or "coldstart"
+LASTSUBDIR=https://fortytwo.cct.lsu.edu/thredds/fileServer/2020/nam/2020102506/HSOFS/supermic.hpc.lsu.edu/HSOFS_nam_jgf_25cm/namforecast
 
-# bde20200919: must coldstart with new +10cm steric adjustment
-COLDSTARTDATE=2020092700
-HOTORCOLD=coldstart
-LASTSUBDIR=null
-#
-# Scenario package
-#
+# Scenario package 
 #PERCENT=default
-SCENARIOPACKAGESIZE=2
+SCENARIOPACKAGESIZE=2 # number of storms in the ensemble
+
 case $si in
  -2)
-   ENSTORM=hindcast
-   ;;
+    ENSTORM=hindcast
+    ;;
  -1)
-   # do nothing ... this is not a forecast
-   ENSTORM=nowcast
-   ;;
+    # do nothing ... this is not a forecast
+    ENSTORM=nowcast
+    ;;
   0)
-   ENSTORM=namforecastWind10m
-   ;;
+    ENSTORM=namforecastWind10m
+    ;;
   1)
-   ENSTORM=namforecast
-   ;;
-*)
+    ENSTORM=namforecast
+    ;;
+ *)
    echo "CONFIGRATION ERROR: Unknown scenario number: '$si'."
    ;;
 esac
 source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on "Wind10m" suffix
-#
 PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz
 HINDCASTARCHIVE=prepped_${GRIDNAME}_hc_${INSTANCENAME}_${NCPU}.tar.gz
+
