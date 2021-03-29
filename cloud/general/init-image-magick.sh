c3e38ce4b49675d91465f7eee878443f26b95c4b
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2021-03-29 18:46:35 +0000

    fixup! working out asgs on debian via Dockerfile

--- cloud/general/init-image-magick.sh
+++ cloud/general/init-image-magick.sh
@@ -53,16 +53,16 @@ cd ImageMagick-$IMAGEMAGICK_VERSION
 
 export CC=gcc
 export MAGICK_HOME=$ASGS_INSTALL_PATH
-export LDFLAGS
-./configure --prefix=$ASGS_INSTALL_PATH --with-gcc-arch=native --with-perl=$(which perl)  \
-	    --without-magick-plus-plus --disable-openmp --enable-shared --with-modules
-make #-j $JOBS
-make install
 
-# Note:
-# these gymnastics are necessary for the Perl module because there is a bug in the supplied Makefile
-# that seems to lose the location of some required shared libraries provided by ImageMagick itself
-cd ./PerlMagick
-perl ./Makefile.PL
+# apply patches to Makefile.PL.in files before ./configure is run
+echo Patching Makefile.PL.in files ...
+patch ./PerlMagick/Makefile.PL.in $SCRIPTDIR/patches/ImageMagick-7/PerlMagic/Makefile.PL.in.patch
+patch ./PerlMagick/default/Makefile.PL.in $SCRIPTDIR/patches/ImageMagick-7/PerlMagic/default/Makefile.PL.in.patch
+patch ./PerlMagick/quantum/Makefile.PL.in $SCRIPTDIR/patches/ImageMagick-7/PerlMagic/quantum/Makefile.PL.in.patch
+
+
+./configure --prefix=$ASGS_INSTALL_PATH --with-gcc-arch=native --with-perl=$(which perl) \
+    --without-magick-plus-plus --disable-openmp --enable-shared --with-modules
+
 make
 make install
