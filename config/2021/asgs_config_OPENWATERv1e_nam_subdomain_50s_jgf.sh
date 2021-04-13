47994fe0efd5978dfcbbb5d170a606e3a441d2d9
Jason Fleming <jason.fleming@seahorsecoastal.com>
2021-04-13 10:31:03 -0500

    Corrected coldstartdate in configs.

--- config/2021/asgs_config_OPENWATERv1e_nam_subdomain_50s_jgf.sh
+++ config/2021/asgs_config_OPENWATERv1e_nam_subdomain_50s_jgf.sh
@@ -89,7 +89,7 @@ TDS=( )
 
 # Initial state (overridden by STATEFILE after ASGS gets going)
 
-COLDSTARTDATE=2021040100
+COLDSTARTDATE=2021032700
 HOTORCOLD=coldstart      # "hotstart" or "coldstart"
 LASTSUBDIR=null
 
