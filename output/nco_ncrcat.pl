1994e21055a2602a668e9cc5354f4ae81672000a
Jason Fleming <jason.fleming@seahorsecoastal.com>
2021-04-27 14:04:52 -0500

    Updated script to enable usage with in-situ fort.61.nc files. Resolves #463.

--- output/nco_ncrcat.pl
+++ output/nco_ncrcat.pl
@@ -23,10 +23,16 @@
 # along with the ASGS.  If not, see <http://www.gnu.org/licenses/>.
 #----------------------------------------------------------------
 # This script needs a "now" date in yyyymmddhh24 format and expects
-# to find directories and files arranged in the following way:
-# ./year/metmodel/yyyymmddhh24/mesh/hpc/asgs_instance/scenario/file
+# to find directories and files arranged in the following way for
+# files posted to an opendap server:
+# $root_data_dir/year/metmodel/yyyymmddhh24/mesh/hpc/asgs_instance/scenario/file
+# and this way for files that are still in-situ in the directories
+# of an asgs instance on the hpc machine:
+# $root_data_dir/yyyymmddhh24/scenario/file
 # For example: 
 #./2021/nam/2021012918/hsofs/hatteras.renci.org/hsofs-nam-bob-2021/nowcast/fort.61.nc
+# or
+# /work/jgflemin/asgs45806/2021042400/nowcast/fort.61.nc
 # Then the script concatenates the output files over different time 
 # periods as follows (in days):
 # 0.25 0.5 1.0 2.0 4.0 8.0 16.0 32.0
@@ -63,14 +69,33 @@ my $csy; my $csm; my $csd; my $csh; # cold start date components
 my @time_periods = qw( 0.25 0.5 1.0 2.0 4.0 8.0 16.0 32.0 );
 my @time_period_prefixes = qw( 6_hours 12_hours 1_day 2_days 4_days 8_days 16_days 32_days );
 my @files_of_interest;       # list of files to concatenate
+my $insitu=0;                # files are still on local hpc (rather than posted to opendap)
 #
 GetOptions(
            "root-data-dir=s" => \$root_data_dir,
+           "in-situ" => \$insitu,
            "target-date=s" => \$target_date
           );
 #
-# find the paths to the files ... the find command does not seem to return them in ascending order
-my @files = `find $root_data_dir -name $datafile -print | sort`;
+# find the paths to the files ... the find command does not seem to return them
+# in ascending order so they are sorted ... only want the nowcast or
+# hindcast scenarios for use in validation
+my @files = `find $root_data_dir -name $datafile -print | grep -E 'nowcast|hindcast' | sort`;
+foreach my $f (@files) {
+   #stderrMessage("DEBUG","$f");
+}
+#
+# check to see if the initialize/hindcast scenario is present; if so,
+# sort will make it the "latest" scenario but we want it to be earliest
+my @path_parts = split("/",$files[-1]);
+foreach my $p (@path_parts) {
+   #stderrMessage("DEBUG","$p");
+}
+#stderrMessage("DEBUG","path_parts[-2] is $path_parts[-2]");
+if ( $path_parts[-2] eq "hindcast" ) {
+   my $hindcast_scenario = pop(@files);
+   unshift(@files,$hindcast_scenario);
+}
 #
 # now reverse the order so it is reverse chronological
 my @reverse_files = reverse(@files);
@@ -88,12 +113,27 @@ if ( $target_date eq "null" ) {
 print "target_date is $target_date\n"; 
 # 
 # go through the list of files and build up the
+# concatenated file
 my @files_in_period; 
+my $datePathLocation = 3; # true if the files were posted to opendap server
+if ( $insitu ) {
+   $datePathLocation = 0; # true if the files are still on the hpc 
+}
 foreach my $f (@reverse_files) {
     my $sub_target_path = substr($f,length($root_data_dir));
+    stderrMessage("DEBUG","sub_target_path $sub_target_path");
     my @target_path_parts = split("/", $sub_target_path) ;
-     # skip files in the list that are later than the target date
-    if ( $target_path_parts[3] > $target_date ) {
+    foreach my $p (@target_path_parts) {
+        #stderrMessage("DEBUG","$p");
+     }
+    # if the sub_target_path starts with / then the 
+    # first element of @target_path_parts will be empty
+    if ( $target_path_parts[0] eq "" ) {
+        shift(@target_path_parts);
+    }
+    # skip files in the list that are later than the target date
+    stderrMessage("DEBUG","target_path_parts[datePathLocation] > target_date ... $target_path_parts[$datePathLocation] > $target_date");
+    if ( $target_path_parts[$datePathLocation] > $target_date ) {
         next;
     }
     # add this file to the list of files of interest (this list is in chronological order)
@@ -139,9 +179,12 @@ foreach my $f (@reverse_files) {
     # at least sometimes 
     if ( $this_file_start_sec < $this_period_start_sec ) {
         my $period_file_list = join(" ",@files_of_interest);
+        #stderrMessage("DEBUG","period_file_list $period_file_list");
         my $period_file_name = $time_period_prefixes[0] . "_fort.61.nc"; 
-        system("ncrcat $period_file_list $period_file_name");
-        print "system(\"ncrcat $period_file_list $period_file_name\");\n";
+        # -O will overwrite an existing file 
+        # -D 0 will set the debug level to 0 and hopefully suppress spurious warnings
+        system("ncrcat -O -D 0 $period_file_list $period_file_name");
+        #stderrMessage("DEBUG","system(ncrcat $period_file_list $period_file_name)");
         # go to the next period
         shift(@time_periods);
         shift(@time_period_prefixes);
