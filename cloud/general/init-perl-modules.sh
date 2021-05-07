70515d72e37dbda8cff0b7da695597544a7a5e17
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2021-05-07 11:47:01 -0500

    smooth out support on rostam

--- cloud/general/init-perl-modules.sh
+++ cloud/general/init-perl-modules.sh
@@ -3,8 +3,20 @@
 # unconditionally attempts to install the Perl modules that are required
 
 PERLBREW_ROOT=${1-$HOME/perl5/perlbrew}
-
 source $PERLBREW_ROOT/etc/bashrc
+
+if [ "${2}" = "clean" ]; then
+  # uninstall tempermental modules first
+  for module in $(cat ./PERL-MODULES.notest); do
+    echo Y | cpanm --uninstall $module
+  done
+
+  # uninstall the rest
+  for module in $(cat ./PERL-MODULES); do
+    echo Y | cpanm --uninstall $module
+  done
+  exit
+fi  
 
 if [ ! -e $PERLBREW_ROOT/bin/cpanm ]; then
   echo installing cpanm...
@@ -13,13 +25,16 @@ fi
 
 echo Installing Perl modules required for ASGS
 
-# due to failing tests that do not affect usefullness
-# the module 
-cpanm install --notest Date::Format
-cpanm install --notest Date::Handler
+MIRROR="https://www.cpan.org/"
 
+# install tempermental modules first
+for module in $(cat ./PERL-MODULES.notest); do
+  cpanm --from $MIRROR --verbose --notest --with-suggests $module
+done
+
+# run tests on these installations
 for module in $(cat ./PERL-MODULES); do
-  cpanm install $module || exit 1 # forces script to exit with error, asgs-brew.pl will catch and report this
+  cpanm --from $MIRROR --verbose --with-suggests $module || exit 1 # forces script to exit with error, asgs-brew.pl will catch and report this
 done
 
 # copy over ./PERL/perltidyrc to $HOME/.perltidyrc
