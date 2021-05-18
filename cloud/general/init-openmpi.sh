f6b1b98571b0ed2900e59b00bf32e0c31bbd7338
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2021-05-17 22:23:10 -0500

    Additional bits added to demonstrate running on rostam.

--- cloud/general/init-openmpi.sh
+++ cloud/general/init-openmpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OPT=${1-$HOME/opt}
-COMPILER=${2-intel}
+COMPILER=${2-gfortran}
 JOBS=${3-1}
 TMP=/tmp/$USER-asgs
 
@@ -10,12 +10,13 @@ OPENMPI_MINOR_VERSION=1
 OPENMPI_FULL_VERSION=openmpi-${OPENMPI_MAJOR_VERSION}.${OPENMPI_MINOR_VERSION}
 
 if [ $2 == "clean" ]; then 
+  COMPILER=gfortran
   echo Cleaning OpenMPI libraries and utilities
-  cd $OPT/bin
+  cd $OPT/$COMPILER/bin
   rm -rfv mpic++ mpic++-vt mpif77-vt mpirun ompi-top orted orte-top otfaux  otfmerge otfshrink vtcc vtfilter  vtrun mpicc mpicxx mpif90 ompi-clean opal_wrapper orte-info oshcc otfcompress otfmerge-mpi shmemcc vtCC vtfiltergen vtunify mpiCC mpicxx-vt mpif90-vt ompi_info opari  orte-ps oshfort otfconfig otfprint shmemfort vtcxx vtfiltergen-mpi vtunify-mpi mpicc-vt mpiexec mpifort ompi-ps ortecc orterun oshmem_info otfdecompress otfprofile shmemrun vtf77 vtfilter-mpi vtwrapper mpiCC-vt mpif77 mpifort-vt ompi-server orte-clean orte-server oshrun otfinfo otfprofile-mpi vtc++ vtf90 vtfort
-  cd $OPT/lib
+  cd $OPT/$COMPILER/lib
  rm -rfv libmca_common_sm.la libmpi_mpifh.so libompitrace.la libopen-rte.so.7 liboshmem.so.1.0.0 libvt-hyb.so.0 libvt-mpi-unify.so libvt.so libmca_common_sm.so libmpi_mpifh.so.2 libompitrace.so libopen-rte.so.7.0.3 libotfaux.a libvt-hyb.so.0.0.0 libvt-mpi-unify.so.0 libvt.so.0 libmca_common_sm.so.4 libmpi_mpifh.so.2.3.0 libompitrace.so.0 libopen-trace-format.a libotfaux.la libvt.la libvt-mpi-unify.so.0.0.0 libvt.so.0.0.0 libmca_common_sm.so.4.0.3 libmpi.so libompitrace.so.0.0.0 libopen-trace-format.la libotfaux.so libvt-mpi.a libvt-mt.a mpi.mod libmpi_cxx.la libmpi.so.1 libopen-pal.la libopen-trace-format.so libotfaux.so.0 libvt-mpi.la libvt-mt.la openmpi libmpi_cxx.so libmpi.so.1.5.0 libopen-pal.so libopen-trace-format.so.1 libotfaux.so.0.0.0 libvt-mpi.so libvt-mt.so pkgconfig libmpi_cxx.so.1 libmpi_usempi.la libopen-pal.so.6 libopen-trace-format.so.1.0.0 libvt.a libvt-mpi.so.0 libvt-mt.so.0 libmpi_cxx.so.1.1.3 libmpi_usempi.so libopen-pal.so.6.1.1 liboshmem.la  libvt-hyb.a libvt-mpi.so.0.0.0 libvt-mt.so.0.0.0 libmpi.la  libmpi_usempi.so.1 libopen-rte.la liboshmem.so  libvt-hyb.la libvt-mpi-unify.a libvt-pomp.a libmpi_mpifh.la libmpi_usempi.so.1.3.0 libopen-rte.so liboshmem.so.1  libvt-hyb.so libvt-mpi-unify.la libvt-pomp.la
-  cd $OPT/include
+  cd $OPT/$COMPILER/include
   rm -rfv mpi-ext.h mpif-constants.h mpif-ext.h mpif-handles.h mpif-io-handles.h mpi.h mpp shmem.fh vampirtrace mpif-config.h mpif-externals.h mpif.h mpif-io-constants.h mpif-sentinels.h mpi_portable_platform.h openmpi shmem.h
   exit
 fi
