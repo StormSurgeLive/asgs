b0a78d743a683defb394018b244cba3811d238c4
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2021-06-19 17:55:59 -0500

    HSOFS for al032021 (CLAUDETTE), NAM

---
+++ config/2021/HSOFS-claudette-al032021.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+#-------------------------------------------------------------------
+# config.sh: This file is read at the beginning of the execution of the ASGS to
+# set up the runs  that follow. It is reread at the beginning of every cycle,
+# every time it polls the datasource for a new advisory. This gives the user
+# the opportunity to edit this file mid-storm to change config parameters
+# (e.g., the name of the queue to submit to, the addresses on the mailing list,
+# etc)
+#-------------------------------------------------------------------
+#
+# Copyright(C) 2020 Jason Fleming
+#
+# This file is part of the ADCIRC Surge Guidance System (ASGS).
+#
+# The ASGS is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# ASGS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# the ASGS.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------
+
+# Fundamental
+
+INSTANCENAME=HSOFS_al032021_bde # "name" of this ASGS process
+ASGSADMIN="asgsnotifications@opayq.com"
+
+# Input files and templates
+
+GRIDNAME=HSOFS
+source $SCRIPTDIR/config/mesh_defaults.sh
+
+# Allocations from which SUs are drawn
+ACCOUNT=hpc_cera_2020b
+#QUEUENAME=priority
+QUEUENAME=workq
+SERQUEUE=single
+
+# Computational Resources (related defaults set in platforms.sh)
+PPN=20
+NCPU=959          # proces for each run
+NCPUCAPACITY=9999 # max number of processors
+NUMWRITERS=1
+
+RMQMessaging_Enable="on"      # "on"|"off"
+RMQMessaging_Transmit="on"    #  enables message transmission ("on" | "off")
+
+# Initial state (overridden by STATEFILE after ASGS gets going)
+COLDSTARTDATE=$(get-coldstart-date)
+HOTORCOLD=hotstart
+LASTSUBDIR=http://tds.renci.org:8080/thredds/fileServer/2021/nam/2021061712/hsofs/hatteras.renci.org/hsofs-nam-bob-2021/nowcast
+
+# Physical forcing (defaults set in config/forcing_defaults.sh)
+
+TIDEFAC=on               # tide factor recalc
+   HINDCASTLENGTH=30.0   # length of initial hindcast, from cold (days)
+BACKGROUNDMET=off         # NAM download/forcing
+   FORECASTCYCLE="00,06,12,18"
+TROPICALCYCLONE=on      # tropical cyclone forcing
+   STORM=03              # storm number, e.g. 05=ernesto in 2006
+   YEAR=2021             # year of the storm
+WAVES=off                # wave forcing
+   REINITIALIZESWAN=no   # used to bounce the wave solution
+VARFLUX=off              # variable river flux forcing
+#STATICOFFSET=0.30
+#
+CYCLETIMELIMIT="99:00:00"
+
+# Post processing and publication
+
+INTENDEDAUDIENCE=general    # "general" | "developers-only" | "professional"
+#POSTPROCESS=( accumulateMinMax.sh createMaxCSV.sh cpra_slide_deck_post.sh includeWind10m.sh createOPeNDAPFileList.sh opendap_post.sh )
+POSTPROCESS=( createMaxCSV.sh includeWind10m.sh createOPeNDAPFileList.sh opendap_post.sh )
+OPENDAPNOTIFY="asgs.cera.lsu@gmail.com,jason.g.fleming@gmail.com,asgsnotifications@opayq.com,cera.asgs.tk@gmail.com,asgsnotes4ian@gmail.com"
+NOTIFY_SCRIPT=ut-nam-notify.sh
+TDS=( lsu_tds ) 
+
+#
+# Scenario package
+#
+#PERCENT=default
+SCENARIOPACKAGESIZE=2 # number of storms in the ensemble
+case $si in
+ -2)
+   ENSTORM=hindcast
+   ;;
+-1)
+   # do nothing ... this is not a forecast
+   ENSTORM=nowcast
+   ;;
+0)
+   ENSTORM=nhcConsensusWind10m
+   ;;
+1)
+   ENSTORM=nhcConsensus
+   ;;
+*)
+   echo "CONFIGRATION ERROR: Unknown scenario number: '$si'."
+   ;;
+esac
+source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on "Wind10m" suffix
+PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz
+HINDCASTARCHIVE=prepped_${GRIDNAME}_hc_${INSTANCENAME}_${NCPU}.tar.gz
