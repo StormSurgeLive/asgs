58acd7f27f5986b344cc85f144a0d7b9783014d2
Jason Fleming <jason.fleming@seahorsecoastal.com>
2021-07-16 15:22:19 -0500

    Updated config to use new hook script format.

--- config/2021/asgs_config_EC95d_nam_jgf.sh
+++ config/2021/asgs_config_EC95d_nam_jgf.sh
@@ -66,9 +66,9 @@ NCPUCAPACITY=9999
 
 # Post processing and publication
 
-INTENDEDAUDIENCE=developers-only    # can also be "developers-only" or "professional"
-FINISH_NOWCAST_SCENARIO=( output/createOPeNDAPFileList.sh output/opendap_post.sh )
-FINISH_SPINUP_SCENARIO=( output/createOPeNDAPFileList.sh output/opendap_post.sh )
+INTENDEDAUDIENCE=general   # can also be "developers-only" or "professional"
+hooksScripts[FINISH_NOWCAST_SCENARIO]=" output/createOPeNDAPFileList.sh output/opendap_post.sh "
+hooksScripts[FINISH_SPINUP_SCENARIO]=" output/createOPeNDAPFileList.sh output/opendap_post.sh "
 POSTPROCESS=( includeWind10m.sh createOPeNDAPFileList.sh opendap_post.sh )
 OPENDAPNOTIFY="jason.g.fleming@gmail.com"
 
