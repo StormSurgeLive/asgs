f18be7d37445ae2a1e2b719d2171fc0d2794a2aa
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2021-07-16 01:03:19 -0500

    Cleaning up anything relying on RMQMessaging_Enable - first pass.

--- monitoring/asgs-msgr.sh
+++ monitoring/asgs-msgr.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#set -x
-#set -e
-#set -u
-
-#export PATH=$PATH:${ADCIRCDIR}:${RMQMessaging_NcoHome}/bin
-
 echo "RMQMessaging:Validating Message Service..."
 
 RMQMessaging_Python=$(which python); # should be using python provided by ASGS environment
@@ -30,23 +24,15 @@ if [[ ! -e ${RMQMessaging_Script_RP} ]] ; then
 	return 1
 fi
 
-#declare -a python_mod_list=("pika" "datetime" "getopt" "yaml" "json" "sys") 
 declare -a python_mod_list=("pika" "datetime" "getopt" "json") 
 for m in "${python_mod_list[@]}"
 do 
-	printf "   Checking for $m ..."
 	${RMQMessaging_Python} -c "import $m" 2> /dev/null
 	if [ "$?" -ne 0 ] ; then 
 		echo "Needed python module $m not found. Turning off RMQMessaging."
 		RMQMessaging_Enable="off"
 		return 1
-	else
-		printf "   Found it.\n"
 	fi
 done
 
-#if [ -d "$RMQMessaging_NcoHome" ] ; then
-
-#FortCheckCommand="/home/ncfs-dev//2014stable/FortCheck_nco.sh"
-
-echo " "
+echo
