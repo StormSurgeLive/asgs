e971308e7fd57c8d617997c26b4a7bd4aa04d8ca
NCFS-DEV <bblanton@renci.org>
2022-03-14 15:55:41 -0400

    pre-penguin push of renci-2021 removed large grid files

---
+++ output/prepare_dwlc_nowcast.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#------------------------------------------------------------------------
+# prepare_dwlc_nowcast: assembles dwlc file from multiple nowcast dwlc files
+#------------------------------------------------------------------------
+# Copyright(C) 2015--2019 Jason Fleming
+#
+# This file is part of the ADCIRC Surge Guidance System (ASGS).
+#
+# The ASGS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ASGS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the ASGS.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------
+#
+THIS=$(basename -- $0)
+#
+declare -A properties
+SCENARIODIR=$PWD
+RUNPROPERTIES=$SCENARIODIR/run.properties
+echo "RUNPROPERTIES=$RUNPROPERTIES"
+
+# get ASGS functions
+SCRIPTDIR=`sed -n 's/[ ^]*$//;s/path.scriptdir\s*:\s*//p' $RUNPROPERTIES`
+echo "SCRIPTDIR=$SCRIPTDIR"
+
+source $SCRIPTDIR/properties.sh
+source $SCRIPTDIR/monitoring/logging.sh
+source $SCRIPTDIR/platforms.sh
+
+if [ ! -e "$RUNPROPERTIES" ] ; then
+    allMessage "prepare_dwlc_nowcast did NOT find the run.properties file."
+    exit 1
+fi
+
+#export ADDAHOME="/home/bblanton/GitHub/RENCI/ADCIRCDataAssimilation"
+#export PYTHONPATH="/home/bblanton/GitHub/RENCI/ADCIRCDataAssimilation"
+#export RUNTIMEDIR="./adda"
+PYTHON="/home/bblanton/miniconda3/bin/python"
+
+#DWLCfile="$RUNTIMEDIR/interpolated/ADCIRC_interpolated_wl.csv"
+#echo "DWLCfile=$DWLCfile"
+
+# load run.properties file into associative array
+loadProperties $RUNPROPERTIES
+gridname=${properties['adcirc.gridname']}
+instancename=${properties['instancename']}
+adv=${properties['advisory']}
+
+Dest="/projects/ncfs/opendap/data/current_"$gridname"_dwlc"
+DestName="$Dest/dwlc_$gridname_$adv.csv"
+DestLinkName="$Dest/dwlc_$gridname.csv"
+
+echo "gridname=$gridname"
+echo "adv=$adv"
+echo "Dest=$Dest"
+echo "DestName=$DestName"
+echo "DestLinkName=$DestLinkName"
+
+NNowcasts=2 # number of nowcasts to merge into one dwlc file
+
+
+#echo $PYTHON $ADDAHOME/ADDA/ADDA.py --grid $gridname --instance $instancename
+#$PYTHON $ADDAHOME/ADDA/ADDA.py --grid $gridname --instance $instancename
+
+#if [ $? == 0 ] ; then
+#
+#    if [ ! -e $DWLCfile ] ; then 
+#        echo "Final ADDA output file DNE.  Terminal." 
+#    else
+#        echo "Pushing $DWLCfile to $Dest ... "
+#        if [ ! -d $Dest ] ; then
+#            mkdir $Dest
+#        fi
+#        cp $DWLCfile "$DestName"
+#        rm -rf "$DestLinkName"
+#        ln -sf "$DestName" "$DestLinkName"
+#    fi 
+#
+#    date > adda.succeeded
+#
+#else
+#    date > adda.failed
+#fi
