1c154d0deac63eb3fab4ad9e83bd60d289728f84
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2022-04-20 12:36:54 -0500

    fixup! Check SCRIPTDIR, warn and adjust if needed.

--- cloud/general/DOT-asgs-brew.sh
+++ cloud/general/DOT-asgs-brew.sh
@@ -253,8 +253,6 @@ load() {
           echo "$W SCRIPTDIR is not the same as your PWD, '$(pwd)'"
           echo "$W  ... reseting ... maybe 'clone profile' to avoid this warning in the future ..."
           export SCRIPTDIR=$(pwd)
-          save profile
-          rl
         fi 
         export PS1="asgs ($_ASGSH_CURRENT_PROFILE)> "
         echo "${I} loaded '$NAME' into current profile"
