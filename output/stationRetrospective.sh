36fd378e352dfdcb9401a7317342c5cd8d1c4aa9
Jason Fleming <jason.fleming@seahorsecoastal.com>
2022-06-26 09:59:57 -0400

    Fixes to dataset counting and sorting file list

--- output/stationRetrospective.sh
+++ output/stationRetrospective.sh
@@ -29,12 +29,17 @@
 # fileList=$(printf "\"%s\",\n" $(ls -d $path | sort))
 # printf "{ \"fort.61.nc\" : [ ${fileList::-1} ] }" | json_pp > fileList.json
 #-----------------------------------------------------------------------
-#
+# april2022=( $(find /mnt/nas-storage/Operations/fortytwo.cct.lsu.edu/2022/nam -name LAv20a -print | grep 202204) )
+# for p in ${april2022[@]}; do echo $p ; nowcastDirs+=( $(find $p -name nowcast -print) ) ; done
+# rm -f tmp ; for d in ${nowcastDirs[@]} ; do echo $d/fort.61.nc >> tmp ; done ; cat tmp | sort -r > filelist.txt
+# stationRetrospective.sh < filelist.txt
+# -----------------------------------------------------------------------
 THIS=$(basename -- $0)
-timePeriods = ( 1 2 4 7 14 21 30 ) # days back from the last available date
+timePeriods=( 1 2 4 7 14 21 30 ) # days back from the last available date
 timePeriodCount=0
 dataSetCount=0
 targetEpochSeconds=0
+declare -a catList  # must ncrcat in chronological order
 declare -a timePeriodsSeconds
 for p in ${timePeriods[@]}; do
     timePeriodsSeconds+=( $(( $p * 86400 )) )
@@ -43,22 +48,29 @@ while read fname
 do
     echo "$fname"
     fileList+=( $fname )
+    catList=( $fname ${catList[@]} )
     base_date=$(ncdump -h $fname | grep base_date | cut -d = -f 2 | tr -d '";')
-    csEpochSeconds=$(TZ=UTC date -u -d "$base_date" "+%s" 2>>$SYSLOG)
-    dataSetSecondsList=( $(ncks --json -v 'time' fort.63.nc | grep data | tr -d '"data:[,]') )
+    csEpochSeconds=$(TZ=UTC date -u -d "$base_date" "+%s")
+    dataSetSecondsList=( $(ncks --json -v 'time' $fname | grep data | tr -d '"data:[,]') )
     numDataSets=$(ncdump -h $fname | grep currently | grep -Eo [0-9]+)
     dataSetCount=$(( $dataSetCount + $numDataSets ))
     declare -a dataSetEpochSecondsList
     for d in ${dataSetSecondsList[@]}; do
         dataSetEpochSecondsList+=( $(( $csEpochSeconds + $d )) )
     done
-    if [[ ${#fileListfileCount[@]} -eq 1 ]]; then
-        targetEpochSeconds=${dataSetEpochSeconds[-1]}
+    echo ${dataSetEpochSecondsList[-1]}
+    if [[ ${#fileList[@]} -eq 1 ]]; then
+        targetEpochSeconds=${dataSetEpochSecondsList[-1]}
+        echo "targetEpochSeconds $targetEpochSeconds"
     fi
     for i in $(seq 1 $numDataSets); do
-        period=$(( $targetEpochSeconds - ${dataSetEpochSecondsList["-$i"]} ))
+        period=$(( $targetEpochSeconds - ${dataSetEpochSecondsList[-$i]} ))
+        #echo $period
         if [[ $period -ge ${timePeriodsSeconds[$timePeriodCount]} ]]; then
-            ncrcat -O -d time,$(( $dataSetCount - $i )),$numDataSets ${$fileList[@]} $(printf "%02d_day.fort.61.nc" ${timePeriods[$timePeriodCount]})
+            retroFileName=$(printf "%02d_day.fort.61.nc" ${timePeriods[$timePeriodCount]})
+            echo
+            echo "ncrcat -O -d time,$(( $numDataSets - $i )),$dataSetCount ${catList[@]} $retroFileName"
+            ncrcat -O -d time,$(( $dataSetCount - $i )),$numDataSets ${catList[@]} $retroFileName
             if [[ $timePeriodCount -lt $(( ${#timePeriods[@]} - 1 )) ]]; then
                 timePeriodCount=$(( $timePeriodCount + 1 ))
             else
