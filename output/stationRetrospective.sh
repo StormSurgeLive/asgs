58c126f1e725b5d3597b6a415c93e0faa875f3a3
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2022-07-21 13:10:32 -0500

    Updates to post processing
    Issue 862: Reduce the number of properties used by
    createMaxCSV.sh
    
    Resolves #862.
    
    Squashed commit of the following:
    
        commit 225ccfdb67e9784782aea864235837e974d9dc1e
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Wed Jul 20 11:33:58 2022 -0400
    
            Created additional notes and comments
    
        commit 5533a4430425aa191a825267eaf90af0f75dec5a
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Sun Jul 17 11:18:42 2022 -0400
    
            Memory allocation fixes for subsetting boundaries
    
        commit af62ce79df626d38d6b6079c790758f791d70cdc
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Sat Jul 16 07:44:05 2022 -0400
    
            Deleted stray file
    
        commit d12bf8d23eaaa9a3f40c766c7814e2ba20a7ebe5
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Sat Jul 16 07:43:50 2022 -0400
    
            Fixed memory allocation and mesh writing bugs when writing netcdf subset meshes
    
        commit 6282140fecc3f49e458fda9a76807574d2d30f02
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Fri Jul 15 08:24:51 2022 -0400
    
            converted unless to if not; used perl scalars for new filehandles
    
        commit 0b85d9b9a58442a5a38d8f69fb01eae7a47bd794
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Thu Jul 14 14:29:59 2022 -0400
    
            Fixes to write out connectivity for levee geometry in adcirc mesh and 2dm mesh formats
    
        commit df1a9a8ba17136ae5532fc9707ef487c2b2159b2
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Thu Jul 7 16:04:26 2022 -0400
    
            Added support for fort.72.nc for met validation
    
        commit 31235b1a4012bf61915e094aa59c08b582d9b510
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Wed Jul 6 10:35:09 2022 -0400
    
            Fixed bug with incrementing split outputs from netcdf2adcirc ; added initial code to support output of levee boundary geometry from adc2vtk.pl
    
        commit 0b9f624a05f13ceab379f3daf5ec9dd4c848c5e5
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Fri Jul 1 16:05:57 2022 -0400
    
            Fixes to incrementing netcdf
    
        commit 47976173dd9c17e8df193dc47a68bce6d899af75
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Mon Jun 27 13:12:22 2022 -0400
    
            Added comments related to querying
    
        commit 364ab0a939c43c28b88ea7197fa611597ea81393
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Sun Jun 26 09:59:57 2022 -0400
    
            Fixes to dataset counting and sorting file list
    
        commit 03250a809fc11a0abe4b0613b76fb7681a19fb93
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Sat Jun 25 15:50:56 2022 -0400
    
            Added station retrospective shell script.
    
        commit 9f5f06aec961d3eec1cf1ac9b056efcf1b306ee6
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Sat Jun 25 14:16:23 2022 -0400
    
            Updates to support time varying CSVs
    
        commit 4bafa6712fdf5f5d5efffad30c68f0604650ce7a
        Author: Jason Fleming <jason.fleming@seahorsecoastal.com>
        Date:   Thu Jun 23 05:20:11 2022 -0400
    
            Improvements for multiple max file types
    
    (cherry picked from commit 231729fa97101ca74f6047777a0012950a02ab78)
    Signed-off-by: wwlwpd <46434714+wwlwpd@users.noreply.github.com>

---
+++ output/stationRetrospective.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#-----------------------------------------------------------------------
+# stationRetrospective.sh : Create a retrospective analysis at a station.
+#-----------------------------------------------------------------------
+# Copyright(C) 2017--2022 Jason Fleming
+#
+# This file is part of the ADCIRC Surge Guidance System (ASGS).
+#
+# The ASGS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ASGS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the ASGS.  If not, see <http://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------
+# This script assumes it is executed within an ASGS shell process
+# and has access to all the normal environmental variables
+# ($SCRIPTDIR, $PATH, etc). It receives a list of fort.61.nc
+# files with fullpath in reverse chronological order on
+# standard input.
+#-----------------------------------------------------------------------
+# path="/mnt/nas-storage/Operations/fortytwo.cct.lsu.edu/2022/nam/202204????/HSOFS/qbc.loni.org/HSOFS_nam_akheir/nowcast"
+# fileList=$(printf "\"%s\",\n" $(ls -d $path | sort))
+# printf "{ \"fort.61.nc\" : [ ${fileList::-1} ] }" | json_pp > fileList.json
+#-----------------------------------------------------------------------
+# april2022=( $(find /mnt/nas-storage/Operations/fortytwo.cct.lsu.edu/2022/nam -name LAv20a -print | grep 202204) )
+# for p in ${april2022[@]}; do echo $p ; nowcastDirs+=( $(find $p -name nowcast -print) ) ; done
+# rm -f tmp ; for d in ${nowcastDirs[@]} ; do echo $d/fort.61.nc >> tmp ; done ; cat tmp | sort -r > filelist.txt
+# stationRetrospective.sh < filelist.txt
+# -----------------------------------------------------------------------
+# cat ../../../../Operations/autodownloader_config_april_nowcast.inventory | grep hsofs-nam-bob-2021 | grep fort.61.nc | sort -r > shortpath_filelist.txt
+# awk '{ print "/mnt/nas-storage/Operations/"$0 }' shortpath_filelist.txt > filelist.txt
+# stationRetrospective.sh < filelist.txt
+# -----------------------------------------------------------------------
+# cat ../../../Operations/autodownloader_config_april_nowcast.inventory | grep NCSC | grep fort.61.nc | sort -r > shortpath_filelist.txt
+# awk '{ print "/mnt/nas-storage/Operations/"$0 }' shortpath_filelist.txt > filelist.txt
+# stationRetrospective.sh < filelist.txt
+# -----------------------------------------------------------------------
+# cat /mnt/nas-storage/Operations/autodownloader_config_april_nowcast.inventory | grep hsofs-nam-bob-2021 | grep fort.72.nc | sort -r > fort.72.shortpath_filelist.txt
+# awk '{ print "/mnt/nas-storage/Operations/"$0 }' fort.72.shortpath_filelist.txt > fort.72.filelist.txt
+# stationRetrospective.sh < fort.72.filelist.txt
+# -----------------------------------------------------------------------
+# export RETROSPECTIVE_TARGET_DATE=2022050100  # set end date for the retrospective
+# -----------------------------------------------------------------------
+# cat autodownloader_config_june_nowcast.inventory | awk 'BEGIN { FS="/" } { print $5 }' | sort | uniq -c -d
+# -----------------------------------------------------------------------
+# fortytwo.cct.lsu.edu/2022/nam/2022070100/LAv20a/qbc.loni.org/LAv20a_nam_akheir_10kcms/nowcast/fort.61.nc
+# fortytwo.cct.lsu.edu/2022/nam/2022063000/LAERDCv5i/supermic.hpc.lsu.edu/LAERDCv5i_nam_jgf_10kcms/nowcast/run.properties
+# fortytwo.cct.lsu.edu/2022/nam/2022060706/EGOMv20b/supermic.hpc.lsu.edu/EGOMv20b_nam_jgf/nowcast/fort.61.nc
+# tds.renci.org/2022/nam/2022060100/NCSC_SAB_v1.23/hatteras.renci.org/ncsc123-nam-sb/nowcast/fort.61.nc
+# tds.renci.org/2022/nam/2022060212/NCSC_SAB_v1.23/hatteras.renci.org/ncsc123-nam-sb55.01/nowcast/fort.61.nc
+# chg-1.oden.tacc.utexas.edu/2022/GFS/2022061806/HSOFS/ls6.tacc.utexas.edu/HSOFS_gfs_jgf/nowcast/fort.61.nc
+# -----------------------------------------------------------------------
+# pat=ncsc123-nam-sb55.01 ; mon=june; for file in fort.61.nc fort.72.nc ; do cat ../../../Operations/autodownloader_config_${mon}_nowcast.inventory | grep $pat | grep $file | sort -r | awk '{ print "/mnt/nas-storage/Operations/"$0 }' | stationRetrospective.sh ; done
+# -----------------------------------------------------------------------
+THIS=$(basename -- $0)
+timePeriods=( 1 2 4 7 14 21 30 ) # days back from the last available date
+timePeriodCount=0
+dataSetCount=0
+targetEpochSeconds=0
+declare -a catList  # must ncrcat in chronological order
+declare -a timePeriodsSeconds
+for p in ${timePeriods[@]}; do
+    timePeriodsSeconds+=( $(( $p * 86400 )) )
+done
+while read fname
+do
+    #echo "$fname"
+    fn=$(basename $fname)
+    fileList+=( $fname )
+    catList=( $fname ${catList[@]} )
+    base_date=$(ncdump -h $fname | grep base_date | cut -d = -f 2 | tr -d '";')
+    csEpochSeconds=$(TZ=UTC date -u -d "$base_date" "+%s")
+    dataSetSecondsList=( $(ncks --json -v 'time' $fname | grep data | tr -d '"data:[,]') )
+    numDataSets=$(ncdump -h $fname | grep currently | grep -Eo [0-9]+)
+    dataSetCount=$(( $dataSetCount + $numDataSets ))
+    declare -a dataSetEpochSecondsList
+    for d in ${dataSetSecondsList[@]}; do
+        dataSetEpochSecondsList+=( $(( $csEpochSeconds + $d )) )
+    done
+    #echo ${dataSetEpochSecondsList[-1]}
+    if [[ ${#fileList[@]} -eq 1 && -z $RETROSPECTIVE_TARGET_DATE ]]; then
+        targetEpochSeconds=${dataSetEpochSecondsList[-1]}
+        echo "targetEpochSeconds $targetEpochSeconds"
+    fi
+    if [[ ! -z $RETROSPECTIVE_TARGET_DATE ]]; then
+        r=$RETROSPECTIVE_TARGET_DATE # for readability of next line
+        targetEpochSeconds=$(TZ=UTC date -u -d "${r:0:4}-${r:4:2}-${r:6:2} ${r:8:2}:00:00" "+%s")
+    fi
+    echo "#!/bin/bash" > $targetEpochSeconds.sh
+    for i in $(seq 1 $numDataSets); do
+        period=$(( $targetEpochSeconds - ${dataSetEpochSecondsList[-$i]} ))
+        #echo $period
+        if [[ $period -ge ${timePeriodsSeconds[$timePeriodCount]} ]]; then
+            retroFileName=$(printf "%02d_day.%s" ${timePeriods[$timePeriodCount]} $fn)
+            #echo "ncrcat -O -d time,$(( $numDataSets - $i )),$dataSetCount ${catList[@]} $retroFileName"
+            ncrcat -O -d time,$(( $numDataSets - $i )),$dataSetCount ${catList[@]} $retroFileName
+            if [[ $timePeriodCount -lt $(( ${#timePeriods[@]} - 1 )) ]]; then
+                timePeriodCount=$(( $timePeriodCount + 1 ))
+            else
+                echo "Finished concatenating data for all time periods."
+                exit 0
+            fi
+        fi
+    done
+    unset dataSetEpochSecondsList
+done < "${1:-/dev/stdin}"
+
+
