bad245b8040af9b39fac4fca56f29e7d2421c9eb
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2022-11-14 01:42:29 -0600

    Refactored bin/init-adcirc.sh to support different sources.
    Issue 1004: "build adcirc" command runs bin/init-adcirc.sh behind
    the scenes. This change adds support for different sources including
    local versions of ADCIRC (provided it can already build) and sources
    that are retreived from the upstream ADCIRC Github repository and
    supports zip archives. In addition to this, patchsets can be added
    for both git and remote zip file based code sources. Patchsets are
    not supported for "custom" source locations intentionally.
    
    The code was also refactored to make it much easier to add support
    for additional sources in the future - with or without patcheset
    support.
    
    Resolves #1004.

--- patches/ADCIRC/v55.01-5bc04d6/info.sh
+++ patches/ADCIRC/v55.01-5bc04d6/info.sh
@@ -1,2 +1,3 @@
+export ADCIRC_SRC_TYPE=git
 export ADCIRC_GIT_BRANCH=5bc04d6
 export SWANDIR=thirdparty/swan
--- patches/ADCIRC/v55.01/info.sh
+++ patches/ADCIRC/v55.01/info.sh
@@ -1,2 +1,3 @@
+export ADCIRC_SRC_TYPE=git
 export ADCIRC_GIT_BRANCH=v55.01
 export SWANDIR=thirdparty/swan
