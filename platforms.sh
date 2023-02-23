1ba3f86776403fa40562632ab6785f7fb61470a5
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2023-02-23 17:03:14 -0600

    Made prompt more configurable and added more useful prompt.
    Issue-1089: Added more useful prompt, updates git branch automatically;
    added colors and created a separate script just for this (etc/PS1.sh).
    
    Default prompt shows,
    
      * updating git repo
      * short hostname
      * current directory
      * ASGS profile loaded
    
    'info' and 'warning' alerts now are colored, where implemented.
    
    To update, just run,
    
      ./update-asgs --update-shell
    
    Resolves #1089.
    Resolves #1090 (PR).
    
    (cherry picked from commit c56824cfb663f40afc64241c32e51c47c0a1df42)
    Signed-off-by: wwlwpd <46434714+wwlwpd@users.noreply.github.com>

--- platforms.sh
+++ platforms.sh
@@ -481,11 +481,11 @@ init_platform()
 {
   local INIT=${1:-"$PLATFORM_INIT"}
   if [ -z "${INIT}" ]; then
-    echo "(warn !!!) platform init script must be specified as the first argument! No platform init.sh known."
+    echo "${W} platform init script must be specified as the first argument! No platform init.sh known."
     return
   fi
   if [ ! -e "${INIT}" ]; then
-    echo "(warn !!!) Can't find init script, '$INIT'! No platform selected."
+    echo "${W} Can't find init script, '$INIT'! No platform selected."
     return
   fi
   printf "... attempting to load '$INIT' ... "
@@ -494,7 +494,7 @@ init_platform()
     printf "OK\n"
   else
     echo
-    echo "(warn !!!) Failed to load '$INIT'! No platform selected."
+    echo "${W} Failed to load '$INIT'! No platform selected."
   fi
   return
 }
@@ -505,7 +505,7 @@ env_dispatch() {
  HPCENVSHORT=$1
  local THIS="platforms.sh>env_dispatch()"
  scenarioMessage "$THIS: Initializing settings for ${HPCENVSHORT}."
- echo "(info)    $THIS: Initializing settings for ${HPCENVSHORT}."
+ echo "${I} $THIS: Initializing settings for ${HPCENVSHORT}."
  case $HPCENVSHORT in
   "queenbee") consoleMessage "$THIS: Queenbee (LONI) configuration found."
           init_queenbee
