e3307407b91a0cb4d3717c6a47a6c1410cc34e09
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2023-04-26 11:09:39 -0500

    Cleaned up location of virtualization supporting files.
    Issue 1110: Updated directory structure for non-critical files
    associated with Docker and VirtualBox set up.
    
    Resolves #1110.

--- cloud/replay-server/make-ssl-certificates.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SSL_DIR=$HOME/nginx/etc/ssl
-FQDN=$(hostname --long)
-mkdir -p $SSL_DIR
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $SSL_DIR/nginx.key -out $SSL_DIR/nginx.crt
--- cloud/replay-server/run-vsftpd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# NOTE: this is used during the building of the Docker container
-
-# Run vsftpd:
-&>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
