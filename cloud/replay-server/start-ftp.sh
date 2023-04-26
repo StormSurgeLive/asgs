f8993f18afa9d4b485bf81c07fb8253ef85b9e01
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2023-04-26 11:09:39 -0500

    Cleaned up location of virtualization supporting files.
    Issue 1110: Updated directory structure for non-critical files
    associated with Docker and VirtualBox set up.
    
    Resolves #1110.

--- cloud/replay-server/start-ftp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# for FTP
-docker run -d -p 20:20 -p 21:21 -p 21100-21110:21100-21110 --restart always -v /home/user/data:/var/ftp:ro vsftpd/anonymous 
--- cloud/replay-server/start-www.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -p 80:80 -p 443:443 -v /home/user/nginx/etc/ssl:/etc/nginx/certs:ro -v /home/user/rss-data:/usr/share/nginx/html:ro --restart always -d nginx-ssl
