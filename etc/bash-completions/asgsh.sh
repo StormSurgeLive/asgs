2852f73eef742e0e7c43075ef91c72efe1dacdf2
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2023-05-23 04:38:22 +0000

    Adding some tools to help with inital set up (config, ssh keys, etc).
    Issue 1139: Added tools, got replay to run without much fuss.
    
    Resolves #1139.
    
    (cherry picked from commit 51141a5c8f36853e7a7606e99043338b5b0f2652)
    Signed-off-by: wwlwpd <46434714+wwlwpd@users.noreply.github.com>

--- etc/bash-completions/asgsh.sh
+++ etc/bash-completions/asgsh.sh
@@ -8,7 +8,7 @@ _goto_completion()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    COMPREPLY=( $(compgen -W "load goto sd rd lsd tailf rebuild clone edit" -- $cur) )
+    COMPREPLY=( $(compgen -W "init load goto sd rd lsd tailf rebuild clone edit" -- $cur) )
     return 0
 } &&
   complete -F _goto_completion goto
