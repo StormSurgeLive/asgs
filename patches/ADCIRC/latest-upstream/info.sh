0a331d712b76e41716240599ca6eeb04b2c99461
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2023-11-09 12:17:57 -0600

    fixup! Updated ADCIRC_GIT_REPO to reflect https rather than ssh.
    Related to #1183.

--- patches/ADCIRC/latest-upstream/info.sh
+++ patches/ADCIRC/latest-upstream/info.sh
@@ -1,5 +1,5 @@
 export ADCIRC_SRC_TYPE=git
-export ADCIRC_GIT_URL=https://github.com/adcirc/
+export ADCIRC_GIT_URL=https://github.com/stormsurgelive
 export ADCIRC_GIT_REPO=adcirc
 export ADCIRC_GIT_BRANCH=development
 export SWANDIR=thirdparty/swan
--- patches/ADCIRC/v55.01/info.sh
+++ patches/ADCIRC/v55.01/info.sh
@@ -1,5 +1,5 @@
 export ADCIRC_SRC_TYPE=git
-export ADCIRC_GIT_URL=https://github.com/adcirc/
+export ADCIRC_GIT_URL=https://github.com/stormsurgelive
 export ADCIRC_GIT_REPO=adcirc
 export ADCIRC_GIT_BRANCH=v55.01
 export SWANDIR=thirdparty/swan
