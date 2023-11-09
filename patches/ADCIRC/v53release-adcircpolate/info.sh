b50a692e583f16267b0f489abde78f1361025d75
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2023-11-08 18:23:48 -0600

    Updated ADCIRC_GIT_REPO to reflect https rather than ssh.
    Issue 1183: The SSH based uri (git@..) requires an ssh key, but https
    does not. This change means that an ssh key is no longer required
    when building from the affected ADCIRC branches hosted on the upstream
    GH repo. Plainly, Github account is no longer needed to build any
    version of ADCIRC within ASGS. Enjoy.
    
    Resolves #1183.
    
    (cherry picked from commit dfaf328c249e3140eed428d28a24fdbe665a6b81)
    Signed-off-by: wwlwpd <46434714+wwlwpd@users.noreply.github.com>

--- patches/ADCIRC/v53release-adcircpolate/info.sh
+++ patches/ADCIRC/v53release-adcircpolate/info.sh
@@ -1,4 +1,5 @@
 export ADCIRC_SRC_TYPE=git
+export ADCIRC_GIT_URL=https://github.com/adcirc/
+export ADCIRC_GIT_REPO=adcirc
 export ADCIRC_GIT_BRANCH=v53release
 export SWANDIR=swan
-export ADCIRC_GIT_URL=git@github.com:adcirc
--- patches/ADCIRC/v55.02/info.sh
+++ patches/ADCIRC/v55.02/info.sh
@@ -1,3 +1,5 @@
 export ADCIRC_SRC_TYPE=git
+export ADCIRC_GIT_URL=https://github.com/adcirc/
+export ADCIRC_GIT_REPO=adcirc
 export ADCIRC_GIT_BRANCH=v55.02
 export SWANDIR=thirdparty/swan
