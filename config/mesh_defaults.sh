a239109b7c11743592fe9507dbee117b2ec6bfe3
Jason Fleming <jason.fleming@seahorsecoastal.com>
2024-02-27 13:37:00 -0500

    Updated LAv20a mesh to new dynamic template system

--- config/mesh_defaults.sh
+++ config/mesh_defaults.sh
@@ -132,20 +132,38 @@ case $GRIDNAME in
       INPUTDIR=$SCRIPTDIR/input/meshes/LA_v20a
       GRIDFILE=LA_v20a-WithUpperAtch_chk.grd   # mesh (fort.14) file
       MESHPROPERTIES=${GRIDFILE}.properties
-      CONTROLTEMPLATE=LA_v20a-WithUpperAtch.15.template
-      # wind at 10m fort.15 template
-      CONTROLTEMPLATENOROUGH=LA_v20a-WithUpperAtch.nowindreduction.15.template
+      CONTROLTEMPLATE=LA_v20a-WithUpperAtch.15.ASGS2024.1.template
       ELEVSTATIONS=combined_stations_20200929.txt
       VELSTATIONS=$ELEVSTATIONS
       METSTATIONS=$ELEVSTATIONS
-      NAFILE=LA_v20a-WithUpperAtch_chk.13
+      # numerics/physics (fort.15)
+      advection="off"                       # on|off for advection (NOLICA=1|0/NOLICAT=1|0)
+      solver_time_integration="implicit"    # implicit|explicit|full-gravity-wave-implicit
+      time_weighting_coefficients="0.35 0.3 0.35" # A00 B00 C00 in fort.15
+      lateral_turbulence="eddy_viscosity"   # "smagorinsky" or "eddy_viscosity"
+      eddy_viscosity_coefficient="50.0"     # ESLM
+      bottom_friction_limit=0.0             # min when using Manning's n (CF/FFACTOR)
+      h0=0.1                                # min depth (m) to be considered wet
+      velmin=0.01
+      nodal_attribute_activate=( surface_directional_effective_roughness_length mannings_n_at_sea_floor )
+      nodal_attribute_activate+=( surface_canopy_coefficient primitive_weighting_in_continuity_equation )
+      nodal_attribute_activate+=( elemental_slope_limiter average_horizontal_eddy_viscosity_in_sea_water_wrt_depth )
+      nodal_attribute_activate+=( surface_submergence_state sea_surface_height_above_geoid )
+      # nodal attributes file
+      NAFILE=LA_v20a-WithUpperAtch_chk.13.template
       nodal_attribute_default_values["sea_surface_height_above_geoid"]="0.228184"
+      nodal_attribute_default_values["surface_submergence_state"]="0.0"
+      nodal_attribute_default_values["primitive_weighting_in_continuity_equation"]="0.03"
+      nodal_attribute_default_values["average_horizontal_eddy_viscosity_in_sea_water_wrt_depth"]="20.0"
+      nodal_attribute_default_values["surface_canopy_coefficient"]="1.0"
+      nodal_attribute_default_values["surface_directional_effective_roughness_length"]="0.0  0.0  0.0 0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0"
+      nodal_attribute_default_values["mannings_n_at_sea_floor"]="0.022"
+      nodal_attribute_default_values["elemental_slope_limiter"]="0.05"
       # intersection between mesh, models, hpc platform, and number of compute cores:
       HINDCASTWALLTIME="18:00:00" # hindcast wall clock time
       ADCPREPWALLTIME="02:00:00"  # adcprep wall clock time, including partmesh
       NOWCASTWALLTIME="07:00:00"  # longest nowcast wall clock time
       FORECASTWALLTIME="07:00:00" # forecast wall clock time
-      # FIXME: no unit offset url
       ;;
    "LAERDCv5k")
       #
