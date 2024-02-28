e79a5119c142058493f4447e53887236e434caf4
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2024-02-28 12:03:05 -0600

    Added build support for noaa.stofs.2d.glo.v2.1.0r1.v55.12
    Issue 1268:
    
    Resolves #1268.

---
+++ patches/ADCIRC/noaa.stofs.2d.glo.v2.1.0r1.v55.12/info.sh
@@ -0,0 +1,5 @@
+export ADCIRC_SRC_TYPE=git
+export ADCIRC_GIT_URL=https://github.com/stormsurgelive
+export ADCIRC_GIT_REPO=adcirc
+export ADCIRC_GIT_BRANCH=noaa.stofs.2d.glo.v2.1.0r1.v55.12
+export SWANDIR=thirdparty/swan
