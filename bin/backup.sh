c21e982910aa8628cad8dd3c4d4a5739be4f18ad
ooder577 <46434714+wwlwpd@users.noreply.github.com>
2024-03-25 01:40:25 -0500

    more tweaking

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,6 +3,7 @@
 PWD=$(pwd)
 PWD=${PWD##*/}
 BKPTGZ=${1:-$HOME/ASGS-SAVE-${PWD}.tgz}
+BKPMD5=${1:-$HOME/ASGS-SAVE-${PWD}.MD5}
 
 # create manifest
 git clean -n -x -d | awk '{print $3}' | grep -v repository > ASGS-MANIFEST
@@ -12,8 +13,8 @@ echo "opt/models/adcircs" >> ASGS-MANIFEST
 echo "git/"               >> ASGS-MANIFEST
 
 # tar/gzip + generation of the contents, each file is checksum'd with MD5
-tar zcvf $BKPTGZ -T ./ASGS-MANIFEST | grep -v '\/$' | xargs md5sum | tee $BKPTGZ.MD5 
+tar zcvf $BKPTGZ -T ./ASGS-MANIFEST | grep -v '\/$' | xargs md5sum | tee $BKPMD5
 
 # compress MD5 manifest
-gzip $BKPTGZ.MD5
+gzip $BKPMD5
 
