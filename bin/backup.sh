c4d70bb31309d20017903dbb4e19405d4ac28d17
ooder577 <46434714+wwlwpd@users.noreply.github.com>
2024-03-25 00:51:13 -0500

    wip backup script

---
+++ bin/backup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PWD=$(pwd)
+PWD=${PWD##*/}
+BKPTGZ=${1:-$HOME/ASGS-SAVE-${PWD}.tgz}
+
+# create manifest
+git clean -n -x -d | awk '{print $3}' | grep -v repository > ASGS-MANIFEST
+
+tar cvzf $BKPTGZ -T ./ASGS-MANIFEST | grep -v '\/$' | xargs md5sum | tee $BKPTGZ.MD5 
+
