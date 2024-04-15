02f2bedeb3584219fad2e9c61b5b97862c3cb21e
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2024-04-15 10:13:20 -0500

    Update info.sh
    No Issue created:
    
    Upstream changed the name of the development branch to `main`. Reflecting this change.

--- patches/ADCIRC/latest-upstream/info.sh
+++ patches/ADCIRC/latest-upstream/info.sh
@@ -1,5 +1,5 @@
 export ADCIRC_SRC_TYPE=git
 export ADCIRC_GIT_URL=https://github.com/adcirc
 export ADCIRC_GIT_REPO=adcirc
-export ADCIRC_GIT_BRANCH=development
+export ADCIRC_GIT_BRANCH=main
 export SWANDIR=thirdparty/swan
