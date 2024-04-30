40322b4b703e1df108ffa530b059e72a589b2e2e
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2024-04-30 12:37:49 -0500

    adding support for v56.0.0-beta.2 on all systems
    Issue 1293: ... also removed latest-stable since that was just there
    as a stop gap; updated latest-upstream to also build (uses main branch
    which is equivalent to v56.0.0-beta.2 at the time of this commit.
    
    Resolves #1293.
    
    (cherry picked from commit faa690385794a65a8110c754a61693389bf3b7b8)
    Signed-off-by: wwlwpd <46434714+wwlwpd@users.noreply.github.com>

---
+++ patches/ADCIRC/v56.0.0-beta.2/info.sh
@@ -0,0 +1,5 @@
+export ADCIRC_SRC_TYPE=git
+export ADCIRC_GIT_URL=https://github.com/adcirc
+export ADCIRC_GIT_REPO=adcirc
+export ADCIRC_GIT_BRANCH=458ddf1773e21676c7bca1aba304358a8fe16b1c
+export SWANDIR=thirdparty/swan
