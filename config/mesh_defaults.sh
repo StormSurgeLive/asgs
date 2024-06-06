e0bea9191195bbdf79ec44af98fae8d638a6927f
Jason Fleming <jason.fleming@seahorsecoastal.com>
2024-06-06 14:52:38 -0400

    Created parameterized NGOMv19 support

--- config/mesh_defaults.sh
+++ config/mesh_defaults.sh
@@ -865,20 +865,59 @@ case $GRIDNAME in
       INPUTDIR=${SCRIPTDIR}/input/meshes/NGOMv19b # grid and other input files
       GRIDFILE=NGOM_RT_v19b_chk.grd
       MESHPROPERTIES=${GRIDFILE}.properties
-      CONTROLTEMPLATE=NGOM_RT_v19b.15.template_18kcms   # fort.15 template
-      # wind at 10m fort.15 template
-      CONTROLTEMPLATENOROUGH=NGOM_RT_v19b.nowindreduction.15.template
       ELEVSTATIONS=NGOM_RT_v19b_stations_08282018.txt
       VELSTATIONS=NGOM_RT_v19b_stations_08282018.txt
       METSTATIONS=NGOM_RT_v19b_stations_08282018.txt
-      NAFILE=NGOM_RT_v19b_chk.13
-      nodal_attribute_default_values["sea_surface_height_above_geoid"]="0.2"
       # intersection between mesh, models, hpc platform, and number of compute cores:
       HINDCASTWALLTIME="18:00:00" # hindcast wall clock time
       ADCPREPWALLTIME="01:00:00"  # adcprep wall clock time, including partmesh
       NOWCASTWALLTIME="10:00:00"  # longest nowcast wall clock time
       FORECASTWALLTIME="07:00:00" # forecast wall clock time
-      UNITOFFSETFILE=oi_surface_NGOM_RT_v19b_chk.grd.dat
+      case $parameterPackage in
+      "hardcoded")
+         CONTROLTEMPLATE=NGOM_RT_v19b.15.template_18kcms   # fort.15 template
+         # wind at 10m fort.15 template
+         CONTROLTEMPLATENOROUGH=NGOM_RT_v19b.nowindreduction.15.template
+         NAFILE=NGOM_RT_v19b_chk.13
+         nodal_attribute_default_values["sea_surface_height_above_geoid"]="0.2"
+      "default")
+         CONTROLTEMPLATE=CPRA23.15.ASGS2024.1.template
+         # numerics/physics (fort.15)
+         advection="off"                       # on|off for advection (NOLICA=1|0/NOLICAT=1|0)
+         solver_time_integration="implicit"    # implicit|explicit|full-gravity-wave-implicit
+         time_weighting_coefficients="0.35 0.3 0.35" # A00 B00 C00 in fort.15
+         lateral_turbulence="eddy_viscosity"   # "smagorinsky" or "eddy_viscosity"
+         eddy_viscosity_coefficient="20.0"     # ESLM
+         bottom_friction_limit=0.0             # min when using Manning's n (CF/FFACTOR)
+         h0=0.1                                # min depth (m) to be considered wet
+         velmin=0.01
+         nodal_attribute_activate+=( sea_surface_height_above_geoid )
+         nodal_attribute_activate+=( elemental_slope_limiter )
+         nodal_attribute_activate+=( surface_submergence_state )
+         nodal_attribute_activate+=( primitive_weighting_in_continuity_equation )
+         nodal_attribute_activate+=( average_horizontal_eddy_viscosity_in_sea_water_wrt_depth )
+         nodal_attribute_activate+=( mannings_n_at_sea_floor )
+         nodal_attribute_activate+=( surface_canopy_coefficient )
+         nodal_attribute_activate+=( surface_directional_effective_roughness_length )
+         # tidal forcing
+         tidalConstituents=( "q1" "o1" "p1" "k1" "n2" "m2" "s2" "k2" )
+         # river boundary forcing
+         PERIODICFLUX=$INPUTDIR/NGOMv19_default_river_flux.txt
+         # nodal attributes file
+         NAFILE=NGOM_RT_v19b_chk.13.template
+         nodal_attribute_default_values["sea_surface_height_above_geoid"]="0.2"
+         nodal_attribute_default_values["elemental_slope_limiter"]="0.02"
+         nodal_attribute_default_values["surface_submergence_state"]="0.0"
+         nodal_attribute_default_values["primitive_weighting_in_continuity_equation"]="0.03"
+         nodal_attribute_default_values["average_horizontal_eddy_viscosity_in_sea_water_wrt_depth"]="2.0"
+         nodal_attribute_default_values["mannings_n_at_sea_floor"]="0.022"
+         nodal_attribute_default_values["surface_canopy_coefficient"]="1.0"
+         nodal_attribute_default_values["surface_directional_effective_roughness_length"]="0.0  0.0  0.0 0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0"
+         ;;
+      *)
+         fatal "The parameter package '$parameterPackage' is not supported for the mesh '$GRIDNAME'."
+         ;;
+      esac
       ;;
       #
       #
