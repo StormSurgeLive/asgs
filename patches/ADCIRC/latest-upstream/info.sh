809b3f10a4e4e39f8b79482aeaef59329ba560ba
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2024-10-07 17:11:27 -0500

    Removed unsupported ADCIRC versions. (#1403)
    Issue 1402: Without sufficient commercial support, the maintenance burden
    is too heavy to maintain all these versions of ADCIRC to the standard we wish
    to provide an SLA for all versions building on all supported platforms.
    
    Users may contact us for custom version support, even strictly in a privated
    and maintanable way using ASGS' "local assets" feature, at 
    
      <help@support.adcirc.live>.
    
    Resolves #1402.

--- patches/ADCIRC/latest-upstream/info.sh
+++
@@ -1,5 +0,0 @@
-export ADCIRC_SRC_TYPE=git
-export ADCIRC_GIT_URL=https://github.com/adcirc
-export ADCIRC_GIT_REPO=adcirc
-export ADCIRC_GIT_BRANCH=main
-export SWANDIR=thirdparty/swan
--- patches/ADCIRC/v55-GLOBAL/info.sh
+++
@@ -1,7 +0,0 @@
-export ADCIRC_SRC_TYPE=remote-zip
-export ADCIRC_BASE_URL=https://zenodo.org/record/3911282/files
-export ADCIRC_SRC_FILE=adcirc-cg-GLOBAL.zip
-export ADCIRC_EXTRACT_DIR=adcirc-cg-GLOBAL
-export ADCIRC_ARCHIVE_URL=${ADCIRC_BASE_URL}/${ADCIRC_SRC_FILE}
-export ADCIRC_EXTRACT_CMD=unzip
-export SWANDIR=swan
