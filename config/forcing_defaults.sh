98387cd443968c2df7440c2987fd7d99b5147382
Jason Fleming <jason.fleming@seahorsecoastal.com>
2025-02-03 10:54:32 -0500

    Updated control file test script so that it works, can be scaled to many tests, and uses code from ASGS for fidelity to Operations

--- config/forcing_defaults.sh
+++ config/forcing_defaults.sh
@@ -31,7 +31,7 @@ tidefac_file="tide_fac.out" # need full path
 tidal_potential_comment="tidal_potential_comment:notset"
 tidal_boundary_comment="tidal_boundary_comment:notset"
 #
-# Meteorology: General
+# Meteorological Forcing: General
 #
 WTIMINC="notset"
 BACKGROUNDMET="on"
@@ -65,6 +65,7 @@ RMAX="default"
 PERCENT="default"
 VORTEXMODEL="GAHM"
 PSEUDOSTORM="no"
+BLADJ=0.9
 #
 #  Meteorological Forcing : North American Mesoscale (NAM) Model
 #
@@ -85,6 +86,7 @@ forecastDownload="only-to-run"        # "only-to-run" or "all"
 GFSBACKSITE="ftp.ncep.noaa.gov"             # forecast data from NCEP
 GFSBACKDIR="/pub/data/nccf/com/gfs/v16.3"   # contains the GFS files
 # GFS subset
+declare -g -A gfsDomain
 gfsDomain['leftlon']='-110'
 gfsDomain['rightlon']='-45'
 gfsDomain['toplat']='50'
@@ -100,12 +102,18 @@ GFSFORECASTLENGTH=${GFSFORECASTLENGTH:-120} # hours of GFS forecast to run
 # nlat = number of latitudes
 # dlon = grid cell size in degrees of longitude
 # dlat = grid cell size in degrees of latitude
+declare -g -A gfsLatLonGrid
 gfsLatLonGrid['lon0']='-100'
 gfsLatLonGrid['nlon']='600'
 gfsLatLonGrid['dlon']='0.1'
 gfsLatLonGrid['lat0']='5'
 gfsLatLonGrid['nlat']='600'
 gfsLatLonGrid['dlat']='0.1'
+#
+# Meteorological Forcing: Wind Blending
+#
+PUREVORTEX=3.0
+PUREBACKGROUND=5.0
 #
 # External data sources : River Flux
 #
