3b1aa51e7957e5bd8f57418df59198a14d983c8b
Jason Fleming <jason.fleming@seahorsecoastal.com>
2025-03-18 12:29:09 -0400

    Updated uncertainty radii for 2025; resolves #1454 (#1459)
    Issue 1454:
    
    Resolves #1454
    Resolves #1459

--- storm_track_gen.pl
+++ storm_track_gen.pl
@@ -779,12 +779,14 @@ sub interpolateUncertaintyRadius($) {
     #my @nhc_radii = (9.5, 32, 52, 71, 90, 122, 170, 225); #2015
     #my @nhc_radii = (9.5, 30, 49, 66, 84, 115, 165, 237); #2016
     #my @nhc_radii = (9.5, 29, 45, 63, 78, 107, 159, 211); #2017
-    #my @nhc_radii =  (9.5, 26, 43, 56, 74, 103, 151, 198); #2018
+    #my @nhc_radii = (9.5, 26, 43, 56, 74, 103, 151, 198); #2018
     #my @nhc_radii = (9.5, 16, 26, 41, 55,  69,  86, 103, 151, 196); # 2020
     #my @nhc_radii = (9.5, 16, 27, 40, 55,  69,  86, 102, 148, 200); # 2021
     #my @nhc_radii = (9.5, 16, 26, 39, 52,  67,  84, 100, 142, 200); # 2022
+    #my @nhc_radii = (9.5, 16, 26, 39, 53,  67,  81,  99, 145, 205); # 2023
+    #my @nhc_radii = (9.5, 16, 26, 41, 55,  70,  88, 102, 151, 220); # 2024 (derived from 2025 numbers and changes)
     my @nhc_tau =   (  0,  3, 12, 24, 36,  48,  60,  72,  96, 120);
-    my @nhc_radii = (9.5, 16, 26, 39, 53,  67,  81,  99, 145, 205); # 2023
+    my @nhc_radii = (9.5, 16, 26, 39, 52,  67,  83, 100, 142, 213);  # 2025 from https://www.nhc.noaa.gov/pdf/NHC_New_Products_Updates_2025.pdf
 
     if ( $tau<$nhc_tau[0] ) {
 	stderrMessage("WARNING","Invalid forecast period (tau) of $tau in fort.22. Setting radius of uncertainty to $nhc_radii[0].");
