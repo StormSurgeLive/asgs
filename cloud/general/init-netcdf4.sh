800281fc064e51a148e7cf663cb2ac34e1918884
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2025-04-11 00:15:53 -0500

    adjusting compilers, latest OneAPI does not include ifort; found an unhandled case in wgrib2
    Issue 1471:
    
    Related to #1471.

--- cloud/general/init-netcdf4.sh
+++ cloud/general/init-netcdf4.sh
@@ -36,8 +36,8 @@ if [ $COMPILER == "intel" ]; then
 fi
 if [ $COMPILER == "intel-oneapi" ]; then
   export CC=icx
-  export FC=ifort
-  export CXX=icx
+  export FC=ifx
+  export CXX=icpx
 fi
 if [ $COMPILER == "gfortran" ]; then
   export CC=gcc
