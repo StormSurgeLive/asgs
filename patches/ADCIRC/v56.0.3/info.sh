aeb0b278382cb1d3a8e2615e615cc8fa37a2307b
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2025-04-13 03:11:03 -0500

    fixup! adjusting compilers, latest OneAPI does not include ifort; found an unhandled case in wgrib2
    Adding v56.0.3, copied directly from v56.0.2 and updated the meta-data
    with the particulars, patches are the same in this commit

---
+++ patches/ADCIRC/v56.0.3/info.sh
@@ -0,0 +1,5 @@
+export ADCIRC_SRC_TYPE=git
+export ADCIRC_GIT_URL=https://github.com/adcirc
+export ADCIRC_GIT_REPO=adcirc
+export ADCIRC_GIT_BRANCH=v56.0.3 # SHA: 147cf546d57530f9f4dd
+export SWANDIR=thirdparty/swan
