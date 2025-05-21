362d6778ccd067920f03f127743e7c12f1f5f9b3
oodler577 <oodler@cpan.org>
2025-05-21 15:00:12 -0500

    gut API stuff, use decoupled CLI client for that via 'fetch'

--- cloud/general/DOT-asgs-brew.sh
+++ cloud/general/DOT-asgs-brew.sh
@@ -75,6 +75,7 @@ help() {
   echo "           statefile       - deletes the state file associated with a profile, effectively for restarting from the initial advisory"
   echo "   dump    <param>         - dumps (using cat) contents specified files: config, exported (variables); and if defined: statefile, syslog"
   echo "   edit    <thing>         - adcirc <NAME>, asgs-global, asgsh-profile, config, jobs, mail-log, meshes, platforms, profile, statefile, syslog"
+  echo "   export-adcirc           - dumps out \$PATH, \$LD_LIBRARY_PATH, and \$LD_INCLUDE_PATH needed to run the built ADCIRC outside of the ASGS Shell"
   echo "   fetch   <thing>         - tool for fetching supported external resources, e.g., git repos; 'fetch' with no parameter will list what is supported"
   echo "   goto|g  <param>         - change CWD to a supported directory. Type 'goto options' to see the currently supported options"
   echo "   guess   platform        - attempts to guess the current platform as supported by platforms.sh (e.g., frontera, supermic, etc)"
@@ -210,12 +211,13 @@ goto() {
 
 # dump envars needed to establish the environment to use the specified
 # version of ADCIRC 
-export_adcirc() {
+export-adcirc() {
   trap 'echo && exit 1' SIGINT
   local adcirc_profile_name=${1:-$ADCIRC_PROFILE_NAME}
   if [[ -n "$adcirc_profile_name" && -e "$ADCIRC_META_DIR/$adcirc_profile_name" ]]; then
     source $ADCIRC_META_DIR/$adcirc_profile_name
     cat <<EOENV
+
 ## copy n' paste this entire BLOCK anywhere ADCIRC must be accessed
 
 #-- BEGIN ADCIRC Live (c) 2024-present exported environment
@@ -227,9 +229,10 @@ export LD_INCLUDE_PATH=$LD_INCLUDE_PATH:\$LD_INCLUDE_PATH
 # Copyright(C) 2024-present Brett Estrade <brett.estrade@adcirc.live>
 # Copyright(C) 2024-present Jason Fleming <jason.fleming@adcirc.live>
 #-- END ADCIRC Live (c) 2024-present exported environment 
+
 EOENV
   else
-    echo "It appears as if ADCIRC must be loaded first. You may email help@support.adcirc.live for assistance."
+    echo "It appears as if ADCIRC is not loaded or built. You may email help@support.adcirc.live for assistance."
   fi
 }
 
