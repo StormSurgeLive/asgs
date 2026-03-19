d6531ed3a4e65cb53d377d62ef7dad889096e69e
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2026-03-19 01:10:20 -0500

    adding support for v56.2.1
    Resolves #1545.

---
+++ patches/ADCIRC/v56.2.1/info.sh
@@ -0,0 +1,5 @@
+export ADCIRC_SRC_TYPE=git
+export ADCIRC_GIT_URL=https://github.com/adcirc
+export ADCIRC_GIT_REPO=adcirc
+export ADCIRC_GIT_BRANCH=v56.2.1 # release tag
+export SWANDIR=thirdparty/swan
