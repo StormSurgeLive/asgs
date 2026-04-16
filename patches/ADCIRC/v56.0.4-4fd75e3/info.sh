c21e246ca6dfe32658bb5ef540983bc16a225f32
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2026-04-16 18:27:46 -0500

    Adding v56.0.4 with memory initialization and output control fixes.
    Issue 1680: This commit SHA is initially pulling from StormSurgeLive's
    ADCIRC fork, SHA #4fd75e3.
    
    Changes have also been pulled in from #1677's branch, which has made
    sure that the correct Intel oneAPI compilers are used depending on the
    system availability of mpiifx or just mpiifort. This includes proper
    detection in the scripts that build HDF5 and netCDF4.
    
    Related to #1671.
    Related to #1677.

---
+++ patches/ADCIRC/v56.0.4-4fd75e3/info.sh
@@ -0,0 +1,5 @@
+export ADCIRC_SRC_TYPE=git
+export ADCIRC_GIT_URL=https://github.com/StormSurgeLive
+export ADCIRC_GIT_REPO=adcirc
+export ADCIRC_GIT_BRANCH=4fd75e3 # commit SHA
+export SWANDIR=thirdparty/swan
