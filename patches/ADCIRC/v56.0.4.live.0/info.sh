38ebec62ef61a9dda417a0a076c79d9ee10219de
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2026-04-18 17:46:53 -0500

    fixup! Adding v56.0.4 with memory initialization and output control fixes.

--- patches/ADCIRC/v56.0.4.live.0/info.sh
+++ patches/ADCIRC/v56.0.4.live.0/info.sh
@@ -1,5 +1,5 @@
 export ADCIRC_SRC_TYPE=git
 export ADCIRC_GIT_URL=https://github.com/StormSurgeLive
 export ADCIRC_GIT_REPO=adcirc
-export ADCIRC_GIT_BRANCH=4fd75e3 # commit SHA
+export ADCIRC_GIT_BRANCH=v56.0.4.live.0
 export SWANDIR=thirdparty/swan
