a122ce2cce7eab59d53ea2ee15afebfd2e7b6418
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2026-04-20 09:07:14 -0500

    Adding v56.0.4 with memory initialization and output control fixes. (#1681)
    Issue 1680: Added v56.0.4 with memory initialization and output control fixes.
    
    Issue 1680: This commit SHA is initially pulling from StormSurgeLive's
    ADCIRC fork, SHA #4fd75e3.
    
    Changes have also been pulled in from #1677's branch, which has made
    sure that the correct Intel oneAPI compilers are used depending on the
    system availability of mpiifx or just mpiifort. This includes proper
    detection in the scripts that build HDF5 and netCDF4.
    
    Related to #1671.
    Related to #1677.
    
    Adding same patch that v56.0.4 has for work/cmplrflags.mk
    
    Adjusting ./thirdparty/swan/macros.inc.gfortran pack to what is in
    v56.0.4 here after patching upstream.

---
+++ patches/ADCIRC/v56.0.4.live.0/info.sh
@@ -0,0 +1,5 @@
+export ADCIRC_SRC_TYPE=git
+export ADCIRC_GIT_URL=https://github.com/StormSurgeLive
+export ADCIRC_GIT_REPO=adcirc
+export ADCIRC_GIT_BRANCH=v56.0.4.live.0
+export SWANDIR=thirdparty/swan
