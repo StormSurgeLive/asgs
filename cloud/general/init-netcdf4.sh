7a748694e4cc4eaaff022975dd340b890d08a2f1
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2026-04-20 09:07:14 -0500

    Adding v56.0.4 with memory initialization and output control fixes. (#1681)
    Issue 1680: Added v56.0.4 with memory initialization and output control fixes.
    
    Issue 1680: This commit SHA is initially pulling from StormSurgeLive's
    ADCIRC fork, SHA #4fd75e3.
    
    Changes have also been pulled in from #1677's branch, which has made
    sure that the correct Intel oneAPI compilers are used depending on the
    system availability of mpiifx or just mpiifort. This includes proper
    detection in the scripts that build HDF5 and netCDF4.
    
    Related to #1671.
    Related to #1677.
    
    Adding same patch that v56.0.4 has for work/cmplrflags.mk
    
    Adjusting ./thirdparty/swan/macros.inc.gfortran pack to what is in
    v56.0.4 here after patching upstream.
    
    (cherry picked from commit a122ce2cce7eab59d53ea2ee15afebfd2e7b6418)
    Signed-off-by: wwlwpd <46434714+wwlwpd@users.noreply.github.com>

--- cloud/general/init-netcdf4.sh
+++ cloud/general/init-netcdf4.sh
@@ -29,20 +29,35 @@ if [ $2 == "clean" ]; then
   exit
 fi
 
-if [ $COMPILER == "intel" ]; then
+# make this consistent with the way we patch ADCIRC's work/cmplrflags.mk
+if [ "$COMPILER" = "intel" ]; then
+  echo "Compiling HDF5 with Intel classic suite"
   export CC=icc
   export FC=ifort
   export CXX=icpc
-fi
-if [ $COMPILER == "intel-oneapi" ]; then
-  export CC=icx
-  export FC=ifx
-  export CXX=icx
-fi
-if [ $COMPILER == "gfortran" ]; then
+
+elif [ "$COMPILER" = "intel-oneapi" ]; then
+  if command -v mpiifx >/dev/null 2>&1; then
+    echo "Compiling HDF5 with Intel oneAPI suite"
+    export CC=icx
+    export FC=ifx
+    export CXX=icpx
+  else
+    echo "mpiifx not found; falling back to Intel classic Fortran toolchain"
+    export CC=icc
+    export FC=ifort
+    export CXX=icpc
+  fi
+
+elif [ "$COMPILER" = "gfortran" ]; then
+  echo "Compiling HDF5 with GCC suite"
   export CC=gcc
   export FC=gfortran
   export CXX=g++
+
+else
+  echo "ERROR: Unknown COMPILER='$COMPILER'"
+  exit 1
 fi
 
 mkdir -p $_ASGS_TMP 2> /dev/null
